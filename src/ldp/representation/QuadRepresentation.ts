e0d74fd68af3575f267f8abc87c51a6fbab28d12
Joachim Van Herwegen <joachimvh@gmail.com>
2020-06-10 11:37:04 +0200

    feat: add typed readable

--- src/ldp/representation/QuadRepresentation.ts
+++ src/ldp/representation/QuadRepresentation.ts
@@ -1,8 +1,11 @@
+import { Quad } from 'rdf-js';
 import { Representation } from './Representation';
+import { TypedReadable } from '../../util/TypedReadable';
 
 /**
  * A representation containing quads as data.
  */
 export interface QuadRepresentation extends Representation {
   dataType: 'quad';
+  data: TypedReadable<Quad>;
 }
