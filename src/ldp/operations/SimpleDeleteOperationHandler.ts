466e7431c7f5c1b788ef889e353fef7920068e51
Joachim Van Herwegen <joachimvh@gmail.com>
2020-06-23 10:18:46 +0200

    feat: add simple operation handlers

---
+++ src/ldp/operations/SimpleDeleteOperationHandler.ts
@@ -0,0 +1,25 @@
+import { Operation } from './Operation';
+import { OperationHandler } from './OperationHandler';
+import { ResourceStore } from '../../storage/ResourceStore';
+import { ResponseDescription } from './ResponseDescription';
+import { UnsupportedHttpError } from '../../util/errors/UnsupportedHttpError';
+
+export class SimpleDeleteOperationHandler extends OperationHandler {
+  private readonly store: ResourceStore;
+
+  public constructor(store: ResourceStore) {
+    super();
+    this.store = store;
+  }
+
+  public async canHandle(input: Operation): Promise<void> {
+    if (input.method !== 'DELETE') {
+      throw new UnsupportedHttpError('This handler only supports DELETE operations.');
+    }
+  }
+
+  public async handle(input: Operation): Promise<ResponseDescription> {
+    await this.store.deleteResource(input.target);
+    return { identifier: input.target };
+  }
+}
---
+++ test/unit/ldp/operations/SimplePostOperationHandler.test.ts
@@ -0,0 +1,22 @@
+import { Operation } from '../../../../src/ldp/operations/Operation';
+import { ResourceIdentifier } from '../../../../src/ldp/representation/ResourceIdentifier';
+import { ResourceStore } from '../../../../src/storage/ResourceStore';
+import { SimplePostOperationHandler } from '../../../../src/ldp/operations/SimplePostOperationHandler';
+import { UnsupportedHttpError } from '../../../../src/util/errors/UnsupportedHttpError';
+
+describe('A SimplePostOperationHandler', (): void => {
+  const store = {
+    addResource: async(): Promise<ResourceIdentifier> => ({ path: 'newPath' } as ResourceIdentifier),
+  } as unknown as ResourceStore;
+  const handler = new SimplePostOperationHandler(store);
+
+  it('only supports POST operations with a body.', async(): Promise<void> => {
+    await expect(handler.canHandle({ method: 'POST', body: { dataType: 'test' }} as Operation)).resolves.toBeUndefined();
+    await expect(handler.canHandle({ method: 'GET', body: { dataType: 'test' }} as Operation)).rejects.toThrow(UnsupportedHttpError);
+    await expect(handler.canHandle({ method: 'POST' } as Operation)).rejects.toThrow(UnsupportedHttpError);
+  });
+
+  it('adds the given representation to the store and returns the new identifier.', async(): Promise<void> => {
+    await expect(handler.handle({ method: 'POST', body: { dataType: 'test' }} as Operation)).resolves.toEqual({ identifier: { path: 'newPath' }});
+  });
+});
