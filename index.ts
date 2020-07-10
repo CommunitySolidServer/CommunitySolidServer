a9dc59bf78393ad6384599ae2f9a901c4b1b6bc2
Joachim Van Herwegen <joachimvh@gmail.com>
2020-07-10 14:23:45 +0200

    feat: Set up server using express

---
+++ index.ts
@@ -0,0 +1,76 @@
+// Authentication
+export * from './src/authentication/Credentials';
+export * from './src/authentication/CredentialsExtractor';
+export * from './src/authentication/SimpleCredentialsExtractor';
+
+// Authorization
+export * from './src/authorization/Authorizer';
+export * from './src/authorization/SimpleAuthorizer';
+
+// LDP/HTTP
+export * from './src/ldp/http/AcceptPreferenceParser';
+export * from './src/ldp/http/BodyParser';
+export * from './src/ldp/http/Patch';
+export * from './src/ldp/http/PreferenceParser';
+export * from './src/ldp/http/RequestParser';
+export * from './src/ldp/http/ResponseWriter';
+export * from './src/ldp/http/SimpleBodyParser';
+export * from './src/ldp/http/SimpleRequestParser';
+export * from './src/ldp/http/SimpleResponseWriter';
+export * from './src/ldp/http/SimpleTargetExtractor';
+export * from './src/ldp/http/TargetExtractor';
+
+// LDP/Operations
+export * from './src/ldp/operations/Operation';
+export * from './src/ldp/operations/OperationHandler';
+export * from './src/ldp/operations/ResponseDescription';
+export * from './src/ldp/operations/SimpleDeleteOperationHandler';
+export * from './src/ldp/operations/SimpleGetOperationHandler';
+export * from './src/ldp/operations/SimplePostOperationHandler';
+
+// LDP/Permissions
+export * from './src/ldp/permissions/PermissionSet';
+export * from './src/ldp/permissions/PermissionsExtractor';
+export * from './src/ldp/permissions/SimplePermissionsExtractor';
+
+// LDP/Representation
+export * from './src/ldp/representation/BinaryRepresentation';
+export * from './src/ldp/representation/NamedRepresentation';
+export * from './src/ldp/representation/QuadRepresentation';
+export * from './src/ldp/representation/Representation';
+export * from './src/ldp/representation/RepresentationMetadata';
+export * from './src/ldp/representation/RepresentationPreference';
+export * from './src/ldp/representation/RepresentationPreferences';
+export * from './src/ldp/representation/ResourceIdentifier';
+
+// LDP
+export * from './src/ldp/AuthenticatedLdpHandler';
+
+// Server
+export * from './src/server/ExpressHttpServer';
+export * from './src/server/HttpHandler';
+export * from './src/server/HttpRequest';
+export * from './src/server/HttpResponse';
+
+// Storage
+export * from './src/storage/AtomicResourceStore';
+export * from './src/storage/Conditions';
+export * from './src/storage/Lock';
+export * from './src/storage/RepresentationConverter';
+export * from './src/storage/ResourceLocker';
+export * from './src/storage/ResourceMapper';
+export * from './src/storage/ResourceStore';
+export * from './src/storage/SimpleResourceStore';
+
+// Util/Errors
+export * from './src/util/errors/HttpError';
+export * from './src/util/errors/NotFoundHttpError';
+export * from './src/util/errors/UnsupportedHttpError';
+export * from './src/util/errors/UnsupportedMediaTypeHttpError';
+
+// Util
+export * from './src/util/AcceptParser';
+export * from './src/util/AsyncHandler';
+export * from './src/util/CompositeAsyncHandler';
+export * from './src/util/TypedReadable';
+export * from './src/util/Util';
---
+++ test/unit/server/ExpressHttpServer.test.ts
@@ -0,0 +1,77 @@
+import { ExpressHttpServer } from '../../../src/server/ExpressHttpServer';
+import { HttpHandler } from '../../../src/server/HttpHandler';
+import { HttpRequest } from '../../../src/server/HttpRequest';
+import { HttpResponse } from '../../../src/server/HttpResponse';
+import request from 'supertest';
+import { Server } from 'http';
+
+const handle = async(input: { request: HttpRequest; response: HttpResponse }): Promise<void> => {
+  input.response.writeHead(200);
+  input.response.end();
+};
+
+class SimpleHttpHandler extends HttpHandler {
+  public async canHandle(): Promise<void> {
+    return undefined;
+  }
+
+  public async handle(input: { request: HttpRequest; response: HttpResponse }): Promise<void> {
+    return handle(input);
+  }
+}
+
+describe('ExpressHttpServer', (): void => {
+  let server: Server;
+  let canHandleJest: jest.Mock<Promise<void>, []>;
+  let handleJest: jest.Mock<Promise<void>, [any]>;
+  beforeEach(async(): Promise<void> => {
+    const handler = new SimpleHttpHandler();
+    canHandleJest = jest.fn(async(): Promise<void> => undefined);
+    handleJest = jest.fn(async(input): Promise<void> => handle(input));
+
+    handler.canHandle = canHandleJest;
+    handler.handle = handleJest;
+
+    const expressServer = new ExpressHttpServer(handler);
+    server = expressServer.listen();
+  });
+
+  afterEach(async(): Promise<void> => {
+    // Close server
+    server.close();
+  });
+
+  it('returns CORS headers for an OPTIONS request.', async(): Promise<void> => {
+    const res = await request(server)
+      .options('/')
+      .set('Access-Control-Request-Headers', 'content-type')
+      .set('Access-Control-Request-Method', 'POST')
+      .set('Host', 'test.com')
+      .expect(204);
+    expect(res.header).toEqual(expect.objectContaining({
+      'access-control-allow-origin': '*',
+      'access-control-allow-headers': 'content-type',
+    }));
+    const corsMethods = res.header['access-control-allow-methods'].split(',').map((method: string): string => method.trim());
+    const allowedMethods = [ 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE' ];
+    expect(corsMethods).toEqual(expect.arrayContaining(allowedMethods));
+    expect(corsMethods.length).toBe(allowedMethods.length);
+  });
+
+  it('specifies CORS origin header if an origin was supplied.', async(): Promise<void> => {
+    const res = await request(server).get('/').set('origin', 'test.com').expect(200);
+    expect(res.header).toEqual(expect.objectContaining({ 'access-control-allow-origin': 'test.com' }));
+  });
+
+  it('sends incoming requests to the handler.', async(): Promise<void> => {
+    await request(server).get('/').set('Host', 'test.com').expect(200);
+    expect(canHandleJest).toHaveBeenCalledTimes(1);
+    expect(handleJest).toHaveBeenCalledTimes(1);
+    expect(handleJest).toHaveBeenLastCalledWith({
+      request: expect.objectContaining({
+        headers: expect.objectContaining({ host: 'test.com' }),
+      }),
+      response: expect.objectContaining({}),
+    });
+  });
+});
