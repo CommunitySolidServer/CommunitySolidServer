d6ff629268b979def2be0fc8ceb03863007a9f42
freyavs <freyavanspeybroeck@outlook.com>
2020-09-04 15:46:36 +0200

    add more fileresourcestore acl tests

--- test/integration/AuthenticatedFileResourceStore.test.ts
+++ test/integration/AuthenticatedFileResourceStore.test.ts
@@ -10,7 +10,7 @@ describe('A server using a FileResourceStore', (): void => {
     const handler = config.getHandler();
     const { store } = config;
 
-    it('can add a file to the store, read it and delete it.', async(): Promise<
+    it('can add a file to the store, read it and delete it if allowed.', async(): Promise<
     void
     > => {
       await setAcl(store,
@@ -56,6 +56,71 @@ describe('A server using a FileResourceStore', (): void => {
       response = await call(handler, requestUrl, 'GET', { accept: 'text/*' }, []);
       expect(response.statusCode).toBe(404);
       expect(response._getData()).toContain('NotFoundHttpError');
+    });
+
+    it('can not add a file to the store if not allowed.', async(): Promise<
+    void
+    > => {
+      await setAcl(store,
+        'http://test.com/',
+        { read: true, write: true, append: true },
+        true,
+        true,
+        true,
+        undefined,
+        'authenticated');
+
+      // POST
+      const requestUrl = new URL('http://test.com/');
+
+      const fileData = await fs.readFile('test/testfiles/testfile1.txt');
+
+      const response: MockResponse<any> = await callFile(
+        handler,
+        requestUrl,
+        'POST',
+        { 'content-type': 'application/octet-stream', slug: 'testfile1.txt', 'transfer-encoding': 'chunked' },
+        fileData,
+      );
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('can not add/delete, but only read files if allowed.', async(): Promise<
+    void
+    > => {
+      await setAcl(store,
+        'http://test.com/',
+        { read: true, write: false, append: false },
+        true,
+        true,
+        true,
+        undefined,
+        'agent');
+
+      // POST
+      let requestUrl = new URL('http://test.com/');
+
+      const fileData = await fs.readFile('test/testfiles/testfile1.txt');
+
+      let response: MockResponse<any> = await callFile(
+        handler,
+        requestUrl,
+        'POST',
+        { 'content-type': 'application/octet-stream', slug: 'testfile1.txt', 'transfer-encoding': 'chunked' },
+        fileData,
+      );
+      expect(response.statusCode).toBe(401);
+
+      // GET
+      requestUrl = new URL('http://test.com/permanent.txt');
+      response = await call(handler, requestUrl, 'GET', { accept: 'text/*' }, []);
+      expect(response.statusCode).toBe(200);
+      expect(response._getHeaders().location).toBe('http://test.com/permanent.txt');
+      expect(response._getBuffer().toString()).toContain('TEST');
+
+      // DELETE
+      response = await call(handler, requestUrl, 'DELETE', {}, []);
+      expect(response.statusCode).toBe(401);
     });
   });
 });
