a2cdeae0f764fc31df156b2c39647cf16241bb2f
thdossch <dossche.thor@gmail.com>
2020-09-10 15:13:51 +0200

    adding test with image

--- test/integration/FileResourceStore.test.ts
+++ test/integration/FileResourceStore.test.ts
@@ -147,5 +147,45 @@ describe('A server using a FileResourceStore', (): void => {
       expect(response.statusCode).toBe(404);
       expect(response._getData()).toContain('NotFoundHttpError');
     });
+
+    it('can upload and delete a image.', async(): Promise<void> => {
+      // POST
+      let requestUrl = new URL('http://test.com');
+      const fileData = await fs.readFile('test/assets/testimage.png');
+      let response: MockResponse<any> = await callFile(
+        handler,
+        requestUrl,
+        'POST',
+        {
+          'content-type': 'application/octet-stream',
+          slug: 'image.png',
+          'transfer-encoding': 'chunked',
+        },
+        fileData,
+      );
+
+      expect(response.statusCode).toBe(200);
+      expect(response._getData()).toHaveLength(0);
+      const id = response._getHeaders().location;
+      expect(id).toContain(url.format(requestUrl));
+
+      // GET
+      requestUrl = new URL(id);
+      response = await call(handler, requestUrl, 'GET', { accept: 'image/png' }, []);
+      expect(response.statusCode).toBe(200);
+      expect(response._getHeaders().location).toBe(id);
+      expect(response._getHeaders()['content-type']).toBe('image/png');
+
+      // DELETE
+      response = await call(handler, requestUrl, 'DELETE', {}, []);
+      expect(response.statusCode).toBe(200);
+      expect(response._getData()).toHaveLength(0);
+      expect(response._getHeaders().location).toBe(url.format(requestUrl));
+
+      // GET
+      response = await call(handler, requestUrl, 'GET', { accept: 'image/png' }, []);
+      expect(response.statusCode).toBe(404);
+      expect(response._getData()).toContain('NotFoundHttpError');
+    });
   });
 });
