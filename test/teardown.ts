9c8fa84c733905591a0f9d15663f9cfecb560052
freyavs <freyavanspeybroeck@outlook.com>
2020-09-10 12:38:48 +0200

    add ignore for CliRunner

--- test/teardown.ts
+++ test/teardown.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
 module.exports = async function(): Promise<void> {
-  // Makes sure that even if tests fail, all test files get removed.
+  // Removes uploads riectory and makes sure that even if tests fail, all test files get removed.
   await fs.rmdir('./uploads', { recursive: true });
 };
