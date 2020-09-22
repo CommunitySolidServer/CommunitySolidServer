ad49b8428325117156c59b2bcd845edfb020a33c
Ruben Taelman <ruben.taelman@ugent.be>
2020-09-22 11:26:53 +0200

    feat: add basic logging interfaces

---
+++ src/logging/LogLevel.ts
@@ -0,0 +1,4 @@
+/**
+ * Different log levels, from most important to least important.
+ */
+export type LogLevel = 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly';
---
+++ src/logging/LoggerFactory.ts
@@ -0,0 +1,12 @@
+import type { Logger } from './Logger';
+
+/**
+ * Instantiates new logger instances.
+ */
+export interface LoggerFactory {
+  /**
+   * Create a logger instance for the given label.
+   * @param label - A label that is used to identify the given logger.
+   */
+  createLogger: (label: string) => Logger;
+}
