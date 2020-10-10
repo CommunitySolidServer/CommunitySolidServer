9dba199f437c94c18a3f961715c9ec982d0a43bf
smessie <smessie@smessie.com>
2020-10-10 12:05:56 +0200

    test: fix Loader mock and add new mock and test

--- test/unit/init/CliRunner.test.ts
+++ test/unit/init/CliRunner.test.ts
@@ -2,39 +2,98 @@ import * as Path from 'path';
 import type { Loader } from 'componentsjs';
 import { runCli } from '../../../src/init/CliRunner';
 import type { Setup } from '../../../src/init/Setup';
+import type { Logger } from '../../../src/logging/Logger';
 
 let calledInstantiateFromUrl: boolean;
 let calledRegisterAvailableModuleResources: boolean;
+let throwError: boolean;
+let outsideResolve: () => void;
+const functionToResolve = new Promise((resolve): any => {
+  outsideResolve = resolve;
+});
 
 const mockSetup = {
   setup: jest.fn(),
 } as unknown as jest.Mocked<Setup>;
 
-// eslint-disable-next-line arrow-body-style
-jest.mock('componentsjs', (): any => {
-  return function(): Loader {
+jest.mock('componentsjs', (): any => (
+  // eslint-disable-next-line @typescript-eslint/naming-convention, object-shorthand
+  { Loader: function(): Loader {
     return {
       instantiateFromUrl(): any {
         calledInstantiateFromUrl = true;
+        if (throwError) {
+          throw new Error('Error! :o');
+        }
         return mockSetup;
       },
       registerAvailableModuleResources(): any {
         calledRegisterAvailableModuleResources = true;
       },
     } as unknown as Loader;
-  };
-});
+  } }
+));
+
+jest.mock('../../../src/logging/LogUtil', (): any => ({
+  getLoggerFor(): Logger {
+    return {
+      info(): any {
+        outsideResolve();
+      },
+    } as unknown as Logger;
+  },
+}));
+
+jest.mock('yargs', (): any => ({
+  usage(): any {
+    return this;
+  },
+  options(): any {
+    return this;
+  },
+  help(): any {
+    // Return once with and once without config value so that both branches are tested.
+    if (throwError) {
+      return {
+        argv: { config: 'value' },
+      };
+    }
+    return {
+      argv: { },
+    };
+  },
+}));
 
 describe('CliRunner', (): void => {
   beforeEach(async(): Promise<void> => {
     calledInstantiateFromUrl = false;
     calledRegisterAvailableModuleResources = false;
+    throwError = false;
   });
 
   it('Runs function for starting the server from the command line.', async(): Promise<void> => {
     runCli(Path.join(__dirname, '..'));
+
+    await functionToResolve;
+
     expect(calledInstantiateFromUrl).toBeTruthy();
     expect(calledRegisterAvailableModuleResources).toBeTruthy();
     expect(mockSetup.setup).toBeCalledTimes(1);
+  });
+
+  it('Writes to stderr when an exception occurs.', async(): Promise<void> => {
+    const mockStderr = jest.spyOn(process.stderr, 'write').mockImplementation((): any => {
+      outsideResolve();
+    });
+
+    throwError = true;
+
+    runCli(Path.join(__dirname, '..'));
+
+    await functionToResolve;
+
+    expect(mockStderr).toHaveBeenCalledTimes(1);
+
+    mockStderr.mockRestore();
   });
 });
