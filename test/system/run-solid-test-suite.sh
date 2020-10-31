bc25c514cdcf8ec435f56209d6a5ff4c321c43d7
Ruben Verborgh <ruben@verborgh.org>
2020-10-31 17:06:58 +0100

    chore: Ensure cleanup always happens.

--- test/system/run-solid-test-suite.sh
+++ test/system/run-solid-test-suite.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
 
 # Start server
 npm start &
@@ -7,7 +6,6 @@ PID=$!
 
 # Initialize tests
 pushd test/tmp
-rm -rf solid-crud-tests
 git clone https://github.com/solid/solid-crud-tests
 cd solid-crud-tests
 git checkout v0.1.1
@@ -19,7 +17,9 @@ export ALICE_WEBID_DOC=$SERVER_ROOT/profile.ttl
 export ALICE_WEBID=$ALICE_WEBID#me
 curl -X PUT $ALICE_WEBID_DOC -d '<#me> <http://www.w3.org/ns/pim/space#storage> </>.'
 npm run jest
+RESULT=$?
 
 # Clean up
 kill $PID
 popd
+exit $RESULT
