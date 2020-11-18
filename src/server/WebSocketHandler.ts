69902c2436dea76d7a3ae2abd6d0e2dfd9ab2e0b
Ruben Verborgh <ruben@verborgh.org>
2020-11-18 23:31:43 +0100

    feat: Add WebSocket functionality to server.

---
+++ src/server/WebSocketHandler.ts
@@ -0,0 +1,9 @@
+import type WebSocket from 'ws';
+import { AsyncHandler } from '../util/AsyncHandler';
+import type { HttpRequest } from './HttpRequest';
+
+/**
+ * A WebSocketHandler handles the communication with multiple WebSockets
+ */
+export abstract class WebSocketHandler
+  extends AsyncHandler<{ webSocket: WebSocket; upgradeRequest: HttpRequest }> {}
---
+++ src/server/WebSocketServerFactory.ts
@@ -0,0 +1,37 @@
+import type { Server } from 'http';
+import type { Socket } from 'net';
+import type WebSocket from 'ws';
+import { Server as WebSocketServer } from 'ws';
+import type { HttpRequest } from './HttpRequest';
+import type { HttpServerFactory } from './HttpServerFactory';
+import type { WebSocketHandler } from './WebSocketHandler';
+
+/**
+ * Factory that adds WebSocket functionality to an existing server
+ */
+export class WebSocketServerFactory implements HttpServerFactory {
+  private readonly baseServerFactory: HttpServerFactory;
+  private readonly webSocketHandler: WebSocketHandler;
+
+  public constructor(baseServerFactory: HttpServerFactory, webSocketHandler: WebSocketHandler) {
+    this.baseServerFactory = baseServerFactory;
+    this.webSocketHandler = webSocketHandler;
+  }
+
+  public startServer(port: number): Server {
+    // Create WebSocket server
+    const webSocketServer = new WebSocketServer({ noServer: true });
+    webSocketServer.on('connection', async(webSocket: WebSocket, upgradeRequest: HttpRequest): Promise<void> => {
+      await this.webSocketHandler.handleSafe({ webSocket, upgradeRequest });
+    });
+
+    // Create base HTTP server
+    const httpServer = this.baseServerFactory.startServer(port);
+    httpServer.on('upgrade', (upgradeRequest: HttpRequest, socket: Socket, head: Buffer): void => {
+      webSocketServer.handleUpgrade(upgradeRequest, socket, head, (webSocket: WebSocket): void => {
+        webSocketServer.emit('connection', webSocket, upgradeRequest);
+      });
+    });
+    return httpServer;
+  }
+}
