e575c62669f0b030805f5f3689c9781af5286581
Ruben Verborgh <ruben@verborgh.org>
2020-11-22 21:58:19 +0100

    chore: Clean up code related to headers.

--- test/unit/ldp/http/BasicTargetExtractor.test.ts
+++ test/unit/ldp/http/BasicTargetExtractor.test.ts
@@ -15,9 +15,19 @@ describe('A BasicTargetExtractor', (): void => {
     await expect(extractor.handle({ url: 'url', headers: {}} as any)).rejects.toThrow('Missing Host header');
   });
 
+  it('errors if the host is invalid.', async(): Promise<void> => {
+    await expect(extractor.handle({ url: 'url', headers: { host: 'test.com/forbidden' }} as any))
+      .rejects.toThrow('The request has an invalid Host header: test.com/forbidden');
+  });
+
   it('returns the input URL.', async(): Promise<void> => {
     await expect(extractor.handle({ url: 'url', headers: { host: 'test.com' }} as any))
       .resolves.toEqual({ path: 'http://test.com/url' });
+  });
+
+  it('supports host:port combinations.', async(): Promise<void> => {
+    await expect(extractor.handle({ url: 'url', headers: { host: 'localhost:3000' }} as any))
+      .resolves.toEqual({ path: 'http://localhost:3000/url' });
   });
 
   it('uses https protocol if the connection is secure.', async(): Promise<void> => {
@@ -26,7 +36,7 @@ describe('A BasicTargetExtractor', (): void => {
     )).resolves.toEqual({ path: 'https://test.com/url' });
   });
 
-  it('encodes relevant characters.', async(): Promise<void> => {
+  it('encodes paths.', async(): Promise<void> => {
     await expect(extractor.handle({ url: '/a%20path%26/name', headers: { host: 'test.com' }} as any))
       .resolves.toEqual({ path: 'http://test.com/a%20path%26/name' });
 
@@ -35,5 +45,10 @@ describe('A BasicTargetExtractor', (): void => {
 
     await expect(extractor.handle({ url: '/path&%26/name', headers: { host: 'test.com' }} as any))
       .resolves.toEqual({ path: 'http://test.com/path%26%26/name' });
+  });
+
+  it('encodes hosts.', async(): Promise<void> => {
+    await expect(extractor.handle({ url: '/', headers: { host: '點看' }} as any))
+      .resolves.toEqual({ path: 'http://xn--c1yn36f/' });
   });
 });
--- test/unit/ldp/http/metadata/LinkRelMetadataWriter.test.ts
+++ test/unit/ldp/http/metadata/LinkRelMetadataWriter.test.ts
@@ -1,27 +1,16 @@
+import { createResponse } from 'node-mocks-http';
 import { LinkRelMetadataWriter } from '../../../../../src/ldp/http/metadata/LinkRelMetadataWriter';
 import { RepresentationMetadata } from '../../../../../src/ldp/representation/RepresentationMetadata';
-import * as util from '../../../../../src/util/HeaderUtil';
 import { LDP, RDF } from '../../../../../src/util/UriConstants';
 import { toNamedNode } from '../../../../../src/util/UriUtil';
 
 describe('A LinkRelMetadataWriter', (): void => {
   const writer = new LinkRelMetadataWriter({ [RDF.type]: 'type', dummy: 'dummy' });
-  let mock: jest.SpyInstance;
-  let addHeaderMock: jest.Mock;
-
-  beforeEach(async(): Promise<void> => {
-    addHeaderMock = jest.fn();
-    mock = jest.spyOn(util, 'addHeader').mockImplementation(addHeaderMock);
-  });
-
-  afterEach(async(): Promise<void> => {
-    mock.mockRestore();
-  });
 
   it('adds the correct link headers.', async(): Promise<void> => {
+    const response = createResponse();
     const metadata = new RepresentationMetadata({ [RDF.type]: toNamedNode(LDP.Resource), unused: 'text' });
-    await expect(writer.handle({ response: 'response' as any, metadata })).resolves.toBeUndefined();
-    expect(addHeaderMock).toHaveBeenCalledTimes(1);
-    expect(addHeaderMock).toHaveBeenLastCalledWith('response', 'link', [ `<${LDP.Resource}>; rel="type"` ]);
+    await expect(writer.handle({ response, metadata })).resolves.toBeUndefined();
+    expect(response.getHeaders()).toEqual({ link: `<${LDP.Resource}>; rel="type"` });
   });
 });
