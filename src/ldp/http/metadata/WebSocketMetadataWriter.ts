4a7ea4ad4692e1a407c4d0a987726d8c142a379f
Ruben Verborgh <ruben@verborgh.org>
2020-11-24 12:53:39 +0100

    feat: Export WebSocket classes.

--- src/ldp/http/metadata/WebSocketMetadataWriter.ts
+++ src/ldp/http/metadata/WebSocketMetadataWriter.ts
@@ -2,15 +2,21 @@ import type { HttpResponse } from '../../../server/HttpResponse';
 import { addHeader } from '../../../util/HeaderUtil';
 import { MetadataWriter } from './MetadataWriter';
 
+interface WebSocketSettings {
+  hostname?: string;
+  port?: number;
+  protocol?: string;
+}
+
 /**
  * A {@link MetadataWriter} that advertises a WebSocket through the Updates-Via header.
  */
 export class WebSocketMetadataWriter extends MetadataWriter {
   private readonly socketUrl: string;
 
-  public constructor({ hostname = 'localhost', port = 80, protocol = 'ws:' }:
-  { hostname?: string; port?: number; protocol?: string }) {
+  public constructor(settings: WebSocketSettings = {}) {
     super();
+    const { hostname = 'localhost', port = 80, protocol = 'ws:' } = settings;
     const secure = /^(?:https|wss)/u.test(protocol);
     const socketUrl = new URL(`${secure ? 'wss' : 'ws'}://${hostname}:${port}/`);
     if (socketUrl.hostname !== hostname) {
