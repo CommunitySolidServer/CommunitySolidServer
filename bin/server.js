d9f18f813e7a1f916e9d23e398c984cac86f4159
Ruben Verborgh <ruben@verborgh.org>
2020-12-13 15:19:44 +0000

    chore: Extend linting to .js files.

--- bin/server.js
+++ bin/server.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
-const { runCli } = require("..");
+const { runCli } = require('..');
 runCli();
--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  "globals": {
-    "ts-jest": {
-      "tsconfig": "tsconfig.json"
-    }
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
   },
-  "transform": {
-    "^.+\\.ts$": "ts-jest"
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
   },
-  "testRegex": "/test/(unit|integration)/.*\\.test\\.ts$",
-  "moduleFileExtensions": [
-    "ts",
-    "js"
+  testRegex: '/test/(unit|integration)/.*\\.test\\.ts$',
+  moduleFileExtensions: [
+    'ts',
+    'js',
   ],
-  "testEnvironment": "node",
-  "setupFilesAfterEnv": ["jest-rdf", "<rootDir>/test/util/SetupTests.ts"],
-  "collectCoverage": true,
-  "coveragePathIgnorePatterns": [
-    "/dist/",
-    "/node_modules/",
-    "/test/"
+  testEnvironment: 'node',
+  setupFilesAfterEnv: [ 'jest-rdf', '<rootDir>/test/util/SetupTests.ts' ],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    '/dist/',
+    '/node_modules/',
+    '/test/',
   ],
-  "coverageThreshold": {
-    "./src": {
-      "branches": 100,
-      "functions": 100,
-      "lines": 100,
-      "statements": 100
-    }
+  coverageThreshold: {
+    './src': {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
   },
-  "testTimeout": 10000, // Slower machines had problems calling the WebSocket integration callbacks on time
+  // Slower machines had problems calling the WebSocket integration callbacks on time
+  testTimeout: 10000,
 };
