2ee2bdd91cfc66db291972c217360be0d3d1f7ca
jaxoncreed <jaxoncreed@gmail.com>
2020-12-13 13:11:33 -0500

    Figured out how to get koa to work

--- src/identity/email-password-identity-provider/EmailPasswordIdentityProviderHandler.ts
+++ src/identity/email-password-identity-provider/EmailPasswordIdentityProviderHandler.ts
@@ -1,22 +1,44 @@
 import type { IncomingMessage, ServerResponse } from 'http';
-import type { Router } from '@bessonovs/node-http-router';
+import Koa from 'koa';
 import { getLoggerFor } from '../../logging/LogUtil';
 import { HttpHandler } from '../../server/HttpHandler';
 import type { Guarded } from '../../util/GuardedStream';
-import getRouter from './tempServer';
 
 interface EmailPasswordIdentityProviderHandlerArgs {
   optional?: string;
 }
 
 export class EmailPasswordIdentityProviderHandler extends HttpHandler {
-  private readonly router: Router;
+  private readonly app: Koa;
   private readonly logger = getLoggerFor(this);
 
   public constructor(args: EmailPasswordIdentityProviderHandlerArgs) {
     super();
     Object.assign(this, args);
-    this.router = getRouter();
+    this.app = new Koa();
+    this.app.onerror = (err): void => {
+      throw err;
+    };
+    this.app.use(
+      async(ctx, next): Promise<void> => {
+        this.logger.info(ctx.url);
+        if (ctx.url === '/cool') {
+          await new Promise((resolve): void => {
+            setTimeout((): void => {
+              ctx.body = 'cool dude';
+              resolve();
+            }, 2000);
+          });
+        } else {
+          return await next();
+        }
+      },
+    );
+    this.app.use(
+      async(): Promise<void> => {
+        throw new Error('oop error');
+      },
+    );
   }
 
   public async handle(input: {
@@ -24,6 +46,10 @@ export class EmailPasswordIdentityProviderHandler extends HttpHandler {
     response: ServerResponse;
   }): Promise<void> {
     this.logger.info('Handling');
-    await this.router.serve(input.request, input.response);
+    const callbackPromise = (this.app.callback()(
+      input.request,
+      input.response,
+    ) as unknown) as Promise<void>;
+    await callbackPromise;
   }
 }
