daeb269e7ff39a9e491ccd7c299dc5d730d474aa
Matthieu Bosquet <matthieubosquet@gmail.com>
2020-12-19 12:34:29 +0000

    chore(deps): Update ts-dpop to v0.2.7.

--- test/__mocks__/ts-dpop.ts
+++ test/__mocks__/ts-dpop.ts
@@ -1,1 +1,4 @@
-export const verify = jest.fn((): string => 'http://alice.example/card#me');
+import type { VerifySolidIdentityFunction } from 'ts-dpop';
+
+const solidIdentityVerifier = jest.fn().mockResolvedValue({ aud: 'solid', exp: 1234, iat: 1234, iss: 'example.com/idp', webid: 'http://alice.example/card#me' });
+export const createSolidIdentityVerifier = jest.fn((): VerifySolidIdentityFunction => solidIdentityVerifier);
