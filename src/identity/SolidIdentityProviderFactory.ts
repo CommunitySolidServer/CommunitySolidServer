fef56d0b7f1aa2e5555bee5a416a83d19aa4dd44
Matthieu Bosquet <matthieubosquet@gmail.com>
2020-12-31 15:25:58 +0000

    fix: Rebase on master

--- src/identity/SolidIdentityProviderFactory.ts
+++ src/identity/SolidIdentityProviderFactory.ts
@@ -1,3 +1,4 @@
+import { TargetExtractor } from '../ldp/http/TargetExtractor';
 import { SolidIdentityProvider } from './SolidIdentityProvider';
 import type { SolidIdentityProviderConfigurationFactory } from './SolidIdentityProviderConfigurationFactory';
 
@@ -9,14 +10,16 @@ export interface SolidIdentityProviderFactoryArgs {
 export class SolidIdentityProviderFactory {
   private readonly configurationFacotry: SolidIdentityProviderConfigurationFactory;
   private readonly issuer: string;
+  private readonly targetExtractor: TargetExtractor;
 
-  public constructor(args: SolidIdentityProviderFactoryArgs) {
+  public constructor(targetExtractor: TargetExtractor, args: SolidIdentityProviderFactoryArgs) {
     this.configurationFacotry = args.configurationFactory;
     this.issuer = args.issuer;
+    this.targetExtractor = targetExtractor;
   }
 
   public async createSolidIdentityProvider(): Promise<SolidIdentityProvider> {
     const configuration = await this.configurationFacotry.createConfiguration();
-    return new SolidIdentityProvider(this.issuer, configuration);
+    return new SolidIdentityProvider(this.targetExtractor, this.issuer, configuration);
   }
 }
