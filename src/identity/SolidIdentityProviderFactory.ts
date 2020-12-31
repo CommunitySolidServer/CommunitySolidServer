b94fb4d7fcb56f53c59ef51b5370bd2786859a83
jaxoncreed <jaxoncreed@gmail.com>
2020-12-31 18:25:55 -0500

    Reimplemented Routing

--- src/identity/SolidIdentityProviderFactory.ts
+++ src/identity/SolidIdentityProviderFactory.ts
@@ -1,26 +1,22 @@
-import { TargetExtractor } from '../ldp/http/TargetExtractor';
 import { SolidIdentityProvider } from './SolidIdentityProvider';
 import type { SolidIdentityProviderConfigurationFactory } from './SolidIdentityProviderConfigurationFactory';
 
 export interface SolidIdentityProviderFactoryArgs {
   configurationFactory: SolidIdentityProviderConfigurationFactory;
   issuer: string;
-  targetExtractor: TargetExtractor;
 }
 
 export class SolidIdentityProviderFactory {
   private readonly configurationFactory: SolidIdentityProviderConfigurationFactory;
   private readonly issuer: string;
-  private readonly targetExtractor: TargetExtractor;
 
   public constructor(args: SolidIdentityProviderFactoryArgs) {
     this.configurationFactory = args.configurationFactory;
     this.issuer = args.issuer;
-    this.targetExtractor = args.targetExtractor;
   }
 
   public createSolidIdentityProvider(): SolidIdentityProvider {
     const configuration = this.configurationFactory.createConfiguration();
-    return new SolidIdentityProvider(this.targetExtractor, this.issuer, configuration);
+    return new SolidIdentityProvider(this.issuer, configuration);
   }
 }
