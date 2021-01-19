dd9d8731226d22e24643ee8565f4369480bae260
Ruben Verborgh <ruben@verborgh.org>
2021-01-19 21:43:33 +0100

    feat: Implement UnsupportedAsyncHandler.

---
+++ src/util/UnsupportedAsyncHandler.ts
@@ -0,0 +1,22 @@
+import { AsyncHandler } from './AsyncHandler';
+import { NotImplementedHttpError } from './errors/NotImplementedHttpError';
+
+/**
+ * Handler that does not support any input and will always throw an error.
+ */
+export class UnsupportedAsyncHandler extends AsyncHandler<any, never> {
+  private readonly errorMessage?: string;
+
+  public constructor(errorMessage?: string) {
+    super();
+    this.errorMessage = errorMessage;
+  }
+
+  public async canHandle(): Promise<never> {
+    throw new NotImplementedHttpError(this.errorMessage);
+  }
+
+  public async handle(): Promise<never> {
+    return this.canHandle();
+  }
+}
