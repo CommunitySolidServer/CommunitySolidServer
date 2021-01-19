4ac0167c8d2b25a5bc5169617f04f2f9f3eece88
Ruben Taelman <ruben.taelman@ugent.be>
2021-01-19 10:14:52 +0100

    Add optional path and url suffixes to FixedContentTypeMapper

--- src/storage/mapping/FixedContentTypeMapper.ts
+++ src/storage/mapping/FixedContentTypeMapper.ts
@@ -1,13 +1,37 @@
 import type { ResourceIdentifier } from '../../ldp/representation/ResourceIdentifier';
+import { NotFoundHttpError } from '../../util/errors/NotFoundHttpError';
 import { NotImplementedHttpError } from '../../util/errors/NotImplementedHttpError';
 import { BaseFileIdentifierMapper } from './BaseFileIdentifierMapper';
+import type { ResourceLink } from './FileIdentifierMapper';
 
+/**
+ * A mapper that always returns a fixed content type for files.
+ */
 export class FixedContentTypeMapper extends BaseFileIdentifierMapper {
   protected readonly contentType: string;
+  protected readonly pathSuffix: string;
+  protected readonly urlSuffix: string;
 
-  public constructor(base: string, rootFilepath: string, contentType: string) {
+  /**
+   * @param base - Base URL.
+   * @param rootFilepath - Base file path.
+   * @param contentType - Fixed content type that will be used for all resources.
+   * @param pathSuffix - An optional suffix that will be appended to all file paths.
+   *                     Requested file paths without this suffix will be rejected.
+   * @param urlSuffix - An optional suffix that will be appended to all URL.
+   *                    Requested URLs without this suffix will be rejected.
+   */
+  public constructor(
+    base: string,
+    rootFilepath: string,
+    contentType: string,
+    pathSuffix = '',
+    urlSuffix = '',
+  ) {
     super(base, rootFilepath);
     this.contentType = contentType;
+    this.pathSuffix = pathSuffix;
+    this.urlSuffix = urlSuffix;
   }
 
   protected async getContentTypeFromUrl(identifier: ResourceIdentifier, contentType?: string): Promise<string> {
@@ -20,5 +44,36 @@ export class FixedContentTypeMapper extends BaseFileIdentifierMapper {
 
   protected async getContentTypeFromPath(): Promise<string> {
     return this.contentType;
+  }
+
+  public async mapUrlToDocumentPath(identifier: ResourceIdentifier, filePath: string, contentType?: string):
+  Promise<ResourceLink> {
+    // Handle URL suffix
+    if (this.urlSuffix) {
+      if (filePath.endsWith(this.urlSuffix)) {
+        filePath = filePath.slice(0, -this.urlSuffix.length);
+      } else {
+        this.logger.warn(`Trying to access URL ${filePath} outside without required suffix ${this.urlSuffix}`);
+        throw new NotFoundHttpError(
+          `Trying to access URL ${filePath} outside without required suffix ${this.urlSuffix}`,
+        );
+      }
+    }
+
+    return super.mapUrlToDocumentPath(identifier, filePath + this.pathSuffix, contentType);
+  }
+
+  protected async getDocumentUrl(relative: string): Promise<string> {
+    // Handle path suffix
+    if (this.pathSuffix) {
+      if (relative.endsWith(this.pathSuffix)) {
+        relative = relative.slice(0, -this.pathSuffix.length);
+      } else {
+        this.logger.warn(`Trying to access file ${relative} outside without required suffix ${this.pathSuffix}`);
+        throw new NotFoundHttpError(`File ${relative} is not part of the file storage at ${this.rootFilepath}`);
+      }
+    }
+
+    return super.getDocumentUrl(relative + this.urlSuffix);
   }
 }
