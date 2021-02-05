d6cdd7dbdfe09d3ae90279d4eca2cb0b9c7d74f9
Joachim Van Herwegen <joachimvh@gmail.com>
2021-02-05 10:57:41 +0100

    feat: Introduce generic auxiliary resource support

---
+++ src/ldp/auxiliary/MetadataGenerator.ts
@@ -0,0 +1,7 @@
+import { AsyncHandler } from '../../util/handlers/AsyncHandler';
+import type { RepresentationMetadata } from '../representation/RepresentationMetadata';
+
+/**
+ * Generic interface for classes that add metadata to a RepresentationMetadata.
+ */
+export abstract class MetadataGenerator extends AsyncHandler<RepresentationMetadata> { }
