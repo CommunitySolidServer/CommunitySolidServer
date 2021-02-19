0ee0af890bab4dcf603bc7131d92861bc42ff648
jaxoncreed <jaxoncreed@gmail.com>
2021-02-19 13:53:27 -0500

    GetPostRouterHandler and BasicOnErrorHandler tests

--- src/server/util/BasicOnErrorHttpHandler.ts
+++ src/server/util/BasicOnErrorHttpHandler.ts
@@ -1,6 +1,7 @@
 import { getLoggerFor } from '../../logging/LogUtil';
 import type { HttpHandlerInput } from '../../server/HttpHandler';
 import { OnErrorHttpHandler } from '../../server/OnErrorHttpHandler';
+import { HttpError } from '../../util/errors/HttpError';
 
 /**
  * A basic OnErrorHttpHandler that simply prints the error.
@@ -10,8 +11,14 @@ export class BasicOnErrorHttpHandler extends OnErrorHttpHandler {
 
   public async handle(input: { error: unknown; input: HttpHandlerInput }): Promise<void> {
     if (input.error instanceof Error) {
+      if (input.error instanceof HttpError) {
+        input.input.response.statusCode = input.error.statusCode;
+      } else {
+        input.input.response.statusCode = 500;
+      }
       input.input.response.end(`${input.error.stack}`);
     } else {
+      input.input.response.statusCode = 500;
       input.input.response.end('Error');
     }
   }
--- test/unit/server/util/GetPostRouterHandler.test.ts
+++ test/unit/server/util/GetPostRouterHandler.test.ts
@@ -1,7 +1,55 @@
+import { createRequest, createResponse } from 'node-mocks-http';
+import type { HttpHandler } from '../../../../src';
+import { guardStream } from '../../../../src';
 import { GetPostRouterHandler } from '../../../../src/server/util/GetPostRouterHandler';
+import { StaticAsyncHandler } from '../../../util/StaticAsyncHandler';
 
 describe('GetPostRouterHandler', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(GetPostRouterHandler).toBeDefined();
+  let getHandler: HttpHandler;
+  let postHandler: HttpHandler;
+  let getHandleFn: jest.Mock<Promise<void>, [any]>;
+  let postHandleFn: jest.Mock<Promise<void>, [any]>;
+
+  beforeEach(async(): Promise<void> => {
+    getHandler = new StaticAsyncHandler<void>(true, undefined);
+    postHandler = new StaticAsyncHandler<void>(true, undefined);
+    getHandleFn = jest.fn(async(input: any): Promise<any> => input);
+    postHandleFn = jest.fn(async(input: any): Promise<any> => input);
+    getHandler.handle = getHandleFn;
+    postHandler.handle = postHandleFn;
+  });
+
+  it('directs to the get handler when there is a get request.', async(): Promise<void> => {
+    const handler = new GetPostRouterHandler([ '/test' ], getHandler, postHandler);
+    const request = guardStream(createRequest({
+      url: '/test',
+      method: 'GET',
+    }));
+    const response = createResponse();
+    await handler.handle({ request, response });
+    expect(getHandleFn).toHaveBeenCalled();
+    expect(postHandleFn).not.toHaveBeenCalled();
+  });
+
+  it('directs to the get handler when there is a post request.', async(): Promise<void> => {
+    const handler = new GetPostRouterHandler([ '/test' ], getHandler, postHandler);
+    const request = guardStream(createRequest({
+      url: '/test',
+      method: 'POST',
+    }));
+    const response = createResponse();
+    await handler.handle({ request, response });
+    expect(postHandleFn).toHaveBeenCalled();
+    expect(getHandleFn).not.toHaveBeenCalled();
+  });
+
+  it('throws an error if the method is not post or get.', async(): Promise<void> => {
+    const handler = new GetPostRouterHandler([ '/test' ], getHandler, postHandler);
+    const request = guardStream(createRequest({
+      url: '/test',
+      method: 'DELETE',
+    }));
+    const response = createResponse();
+    await expect(handler.handle({ request, response })).rejects.toThrow('Cannot DELETE to this route.');
   });
 });
