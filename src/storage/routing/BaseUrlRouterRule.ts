ea6d8bcd5a1166357c32ab3ef169db3235bc2890
Joachim Van Herwegen <joachimvh@gmail.com>
2021-02-25 17:05:54 +0100

    feat: Create router rule based on matching the base URL

---
+++ src/storage/routing/BaseUrlRouterRule.ts
@@ -0,0 +1,48 @@
+import type { ResourceIdentifier } from '../../ldp/representation/ResourceIdentifier';
+import { NotFoundHttpError } from '../../util/errors/NotFoundHttpError';
+import type { KeyValueStorage } from '../keyvalue/KeyValueStorage';
+import type { ResourceStore } from '../ResourceStore';
+import { RouterRule } from './RouterRule';
+
+/**
+ * Routes requests based on their base url.
+ * Checks if any of the stored base URLs match the request identifier.
+ * If there are no matches the base store will be returned if one was configured.
+ *
+ * Part of the dynamic pod creation.
+ * Uses the identifiers that were added to the routing storage.
+ * @see {@link TemplatedPodGenerator}, {@link ConfigPodInitializer}, {@link ConfigPodManager}
+ */
+export class BaseUrlRouterRule extends RouterRule {
+  private readonly baseStore?: ResourceStore;
+  private readonly stores: KeyValueStorage<ResourceIdentifier, ResourceStore>;
+
+  public constructor(stores: KeyValueStorage<ResourceIdentifier, ResourceStore>, baseStore?: ResourceStore) {
+    super();
+    this.baseStore = baseStore;
+    this.stores = stores;
+  }
+
+  public async handle({ identifier }: { identifier: ResourceIdentifier }): Promise<ResourceStore> {
+    try {
+      return await this.findStore(identifier);
+    } catch (error: unknown) {
+      if (this.baseStore) {
+        return this.baseStore;
+      }
+      throw error;
+    }
+  }
+
+  /**
+   * Finds the store whose base url key is contained in the given identifier.
+   */
+  private async findStore(identifier: ResourceIdentifier): Promise<ResourceStore> {
+    for await (const [ key, store ] of this.stores.entries()) {
+      if (identifier.path.startsWith(key.path)) {
+        return store;
+      }
+    }
+    throw new NotFoundHttpError();
+  }
+}
