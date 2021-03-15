3d042bcad83d3f012e4a776c36c5eefa616b9c20
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-15 16:32:09 +0100

    test: Remove unnecessary test file

--- test/unit/identity/interaction/email-password/handler/EmailPasswordResetPasswordRenderHandler.test.ts
+++
@@ -1,9 +0,0 @@
-import {
-  EmailPasswordResetPasswordRenderHandler,
-} from '../../../../../../src/identity/interaction/email-password/handler/EmailPasswordResetPasswordRenderHandler';
-
-describe('EmailPasswordResetPasswordRenderHandler', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(EmailPasswordResetPasswordRenderHandler).toBeDefined();
-  });
-});
