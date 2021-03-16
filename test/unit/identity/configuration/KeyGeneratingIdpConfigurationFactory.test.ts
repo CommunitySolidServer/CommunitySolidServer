0128c96b524a2f1fa9aa7f8a97986ef5e3961443
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-16 16:17:19 +0100

    test: Add KeyGeneratingIdpConfigurationGenerator unit tests

--- test/unit/identity/configuration/KeyGeneratingIdpConfigurationFactory.test.ts
+++
@@ -1,9 +0,0 @@
-import {
-  KeyGeneratingIdpConfigurationGenerator,
-} from '../../../../src/identity/configuration/KeyGeneratingIdpConfigurationGenerator';
-
-describe('KeyGeneratingIdpConfigurationGenerator', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(KeyGeneratingIdpConfigurationGenerator).toBeDefined();
-  });
-});
