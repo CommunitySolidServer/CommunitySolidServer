d865450892ba27f4ade9815a9549f356c931e9e0
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-23 11:20:38 +0100

    test: Add EmailPasswordInteractionPolicy unit tests

--- test/unit/identity/interaction/email-password/EmailPasswordInteractionPolicy.test.ts
+++ test/unit/identity/interaction/email-password/EmailPasswordInteractionPolicy.test.ts
@@ -2,8 +2,15 @@ import {
   EmailPasswordInteractionPolicy,
 } from '../../../../../src/identity/interaction/email-password/EmailPasswordInteractionPolicy';
 
-describe('EmailPasswordInteractionPolicyHttpHandler', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(EmailPasswordInteractionPolicy).toBeDefined();
+describe('An EmailPasswordInteractionPolicy', (): void => {
+  const interactionPolicy = new EmailPasswordInteractionPolicy();
+
+  it('has a select_account policy at index 0.', async(): Promise<void> => {
+    expect(interactionPolicy.policy[0].name).toBe('select_account');
+  });
+
+  it('creates URLs by prepending /idp/interaction/.', async(): Promise<void> => {
+    await expect(interactionPolicy.url({ oidc: { uid: 'valid-uid' }} as any))
+      .resolves.toBe('/idp/interaction/valid-uid');
   });
 });
