da4bdae3f4b64082ec55b63c790c6769bc28b73f
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-23 15:26:03 +0100

    test: Add OidcInteractionCompleter unit tests

--- test/unit/identity/interaction/util/OidcInteractionCompleter.test.ts
+++ test/unit/identity/interaction/util/OidcInteractionCompleter.test.ts
@@ -1,9 +1,52 @@
+import type { Provider } from 'oidc-provider';
 import {
   OidcInteractionCompleter,
 } from '../../../../../src/identity/interaction/util/OidcInteractionCompleter';
+import type { HttpRequest } from '../../../../../src/server/HttpRequest';
+import type { HttpResponse } from '../../../../../src/server/HttpResponse';
 
 describe('OidcInteractionCompleter', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(OidcInteractionCompleter).toBeDefined();
+  const request: HttpRequest = 'request!' as any;
+  const response: HttpResponse = 'response!' as any;
+  const webId = 'http://alice.test.com/#me';
+  let provider: Provider;
+  const completer = new OidcInteractionCompleter();
+
+  beforeEach(async(): Promise<void> => {
+    provider = {
+      interactionFinished: jest.fn(),
+    } as any;
+  });
+
+  it('sends the correct data to the provider.', async(): Promise<void> => {
+    await expect(completer.handle({ request, response, provider, webId, shouldRemember: true }))
+      .resolves.toBeUndefined();
+    expect(provider.interactionFinished).toHaveBeenCalledTimes(1);
+    expect(provider.interactionFinished).toHaveBeenLastCalledWith(request, response, {
+      login: {
+        account: webId,
+        remember: true,
+        ts: Math.floor(Date.now() / 1000),
+      },
+      consent: {
+        rejectedScopes: [],
+      },
+    });
+  });
+
+  it('rejects offline access if shouldRemember is false.', async(): Promise<void> => {
+    await expect(completer.handle({ request, response, provider, webId, shouldRemember: false }))
+      .resolves.toBeUndefined();
+    expect(provider.interactionFinished).toHaveBeenCalledTimes(1);
+    expect(provider.interactionFinished).toHaveBeenLastCalledWith(request, response, {
+      login: {
+        account: webId,
+        remember: false,
+        ts: Math.floor(Date.now() / 1000),
+      },
+      consent: {
+        rejectedScopes: [ 'offline_access' ],
+      },
+    });
   });
 });
