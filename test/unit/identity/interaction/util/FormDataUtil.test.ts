e33af6faa4b105a92f99b648bb7e2225c1ae7d72
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-23 13:46:02 +0100

    test: Add FormDataUtil unit tests

--- test/unit/identity/interaction/util/FormDataUtil.test.ts
+++ test/unit/identity/interaction/util/FormDataUtil.test.ts
@@ -1,9 +1,23 @@
+import { stringify } from 'querystring';
 import {
   getFormDataRequestBody,
 } from '../../../../../src/identity/interaction/util/FormDataUtil';
+import type { HttpRequest } from '../../../../../src/server/HttpRequest';
+import { UnsupportedMediaTypeHttpError } from '../../../../../src/util/errors/UnsupportedMediaTypeHttpError';
+import { guardedStreamFrom } from '../../../../../src/util/StreamUtil';
 
 describe('FormDataUtil', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(getFormDataRequestBody).toBeDefined();
+  describe('#getFormDataRequestBody', (): void => {
+    it('only supports form data.', async(): Promise<void> => {
+      await expect(getFormDataRequestBody({ headers: { 'content-type': 'text/turtle' }} as any))
+        .rejects.toThrow(UnsupportedMediaTypeHttpError);
+    });
+
+    it('converts the body to an object.', async(): Promise<void> => {
+      const data = { test: 'test!', moreTest: '!TEST!' };
+      const stream = guardedStreamFrom(stringify(data)) as HttpRequest;
+      stream.headers = { 'content-type': 'application/x-www-form-urlencoded' };
+      await expect(getFormDataRequestBody(stream)).resolves.toEqual(data);
+    });
   });
 });
