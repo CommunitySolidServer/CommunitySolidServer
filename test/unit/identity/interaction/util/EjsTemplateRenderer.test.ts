020278f9382454832473d5e496f8f8d37e33c631
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-23 13:22:37 +0100

    test: Add EjsTemplateRenderer unit tests

--- test/unit/identity/interaction/util/EjsTemplateRenderer.test.ts
+++ test/unit/identity/interaction/util/EjsTemplateRenderer.test.ts
@@ -1,9 +1,19 @@
+import { renderFile } from 'ejs';
 import {
   EjsTemplateRenderer,
 } from '../../../../../src/identity/interaction/util/EjsTemplateRenderer';
 
-describe('EjsTemplateRenderer', (): void => {
-  it('satisfies a trivial use case.', async(): Promise<void> => {
-    expect(EjsTemplateRenderer).toBeDefined();
+jest.mock('ejs');
+
+describe('An EjsTemplateRenderer', (): void => {
+  const templatePath = '/var/templates/';
+  const templateFile = 'template.ejs';
+  const options = 'options!';
+  const renderer = new EjsTemplateRenderer<string>(templatePath, templateFile);
+
+  it('renders the given file with the given options.', async(): Promise<void> => {
+    await expect(renderer.render(options)).resolves.toBeUndefined();
+    expect(renderFile).toHaveBeenCalledTimes(1);
+    expect(renderFile).toHaveBeenLastCalledWith('/var/templates/template.ejs', options);
   });
 });
