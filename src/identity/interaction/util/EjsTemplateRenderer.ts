55a1e051939eeff74a6002dca98a1ba4c8d04598
Joachim Van Herwegen <joachimvh@gmail.com>
2021-03-29 15:18:51 +0200

    refactor: Use interfaces instead of abstract classes

--- src/identity/interaction/util/EjsTemplateRenderer.ts
+++ src/identity/interaction/util/EjsTemplateRenderer.ts
@@ -1,18 +1,17 @@
 import { renderFile } from 'ejs';
 import { joinFilePath } from '../../../util/PathUtil';
-import { TemplateRenderer } from './TemplateRenderer';
+import type { TemplateRenderer } from './TemplateRenderer';
 
 /**
  * Renders options using a given EJS template location and
  * returns the result as a string. This is good for rendering
  * emails.
  */
-export class EjsTemplateRenderer<T> extends TemplateRenderer<T> {
+export class EjsTemplateRenderer<T> implements TemplateRenderer<T> {
   private readonly templatePath: string;
   private readonly templateFile: string;
 
   public constructor(templatePath: string, templateFile: string) {
-    super();
     this.templatePath = templatePath;
     this.templateFile = templateFile;
   }
--- src/identity/interaction/util/WebIdOwnershipValidator.ts
+++ src/identity/interaction/util/WebIdOwnershipValidator.ts
@@ -2,6 +2,6 @@
  * A class that validates if a someone owns a WebId. Will
  * throw an error if the WebId is not valid.
  */
-export abstract class WebIdOwnershipValidator {
-  abstract assertWebIdOwnership(webId: string, interactionId: string): Promise<void>;
+export interface WebIdOwnershipValidator {
+  assertWebIdOwnership: (webId: string, interactionId: string) => Promise<void>;
 }
