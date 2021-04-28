8f3fc73c6dcd5c70983b58bc451147c6ae4dede7
Ruben Taelman <ruben.taelman@ugent.be>
2021-04-28 09:24:18 +0200

    fix: process being passed incorrectly to runCli

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const { AppRunner } = require('..');
-new AppRunner(process).runCli();
+new AppRunner().runCli(process);
