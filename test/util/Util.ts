ec8f77e47e6fa5fa627a2d715df31ca337d04a14
Joachim Van Herwegen <joachimvh@gmail.com>
2021-04-29 14:58:13 +0200

    test: Update LdpHandlerWithoutAuth to use fetch for tests

--- test/util/Util.ts
+++ test/util/Util.ts
@@ -12,6 +12,7 @@ import type { SystemError } from '../../src/util/errors/SystemError';
 /* eslint-disable @typescript-eslint/naming-convention */
 const portNames = [
   'DynamicPods',
+  'LpdHandlerWithoutAuth',
   'Middleware',
   'PodCreation',
   'RedisResourceLocker',
