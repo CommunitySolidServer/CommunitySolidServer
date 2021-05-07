8491300f427bb066c55ff04053a549b8bb3af42b
Joachim Van Herwegen <joachimvh@gmail.com>
2021-05-07 10:31:21 +0200

    feat: Pass ResourceStore as param to PatchHandler
    This way the chain of ResourceStores is a bit easier to configure.
    This commit also updates the SparqlUpdatePatchHandler to keep the metadata
    and content-type of the resource that is being modified.

--- src/storage/patch/PatchHandler.ts
+++ src/storage/patch/PatchHandler.ts
@@ -1,6 +1,16 @@
 import type { Patch } from '../../ldp/http/Patch';
 import type { ResourceIdentifier } from '../../ldp/representation/ResourceIdentifier';
 import { AsyncHandler } from '../../util/handlers/AsyncHandler';
+import type { ResourceStore } from '../ResourceStore';
 
-export abstract class PatchHandler
-  extends AsyncHandler<{identifier: ResourceIdentifier; patch: Patch}, ResourceIdentifier[]> {}
+export type PatchHandlerArgs<T extends ResourceStore = ResourceStore> = {
+  source: T;
+  identifier: ResourceIdentifier;
+  patch: Patch;
+};
+
+/**
+ * Executes the given Patch.
+ */
+export abstract class PatchHandler<T extends ResourceStore = ResourceStore>
+  extends AsyncHandler<PatchHandlerArgs<T>, ResourceIdentifier[]> {}
