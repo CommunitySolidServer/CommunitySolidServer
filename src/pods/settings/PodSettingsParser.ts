130a91fdfb12210d102ecba8f4f409ecbd3719bd
Joachim Van Herwegen <joachimvh@gmail.com>
2021-05-28 16:39:28 +0200

    fix: Remove unneeded pod files

--- src/pods/settings/PodSettingsParser.ts
+++
@@ -1,8 +0,0 @@
-import type { Representation } from '../../ldp/representation/Representation';
-import { AsyncHandler } from '../../util/handlers/AsyncHandler';
-import type { PodSettings } from './PodSettings';
-
-/**
- * Parser that generates a {@link PodSettings} from the data in the given {@link Representation}.
- */
-export abstract class PodSettingsParser extends AsyncHandler<Representation, PodSettings> { }
