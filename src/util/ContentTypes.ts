a5dfe070fe1515c0169b7291bd0200300563ba6f
Joachim Van Herwegen <joachimvh@gmail.com>
2021-06-07 14:44:19 +0200

    feat: Add ErrorToHtmlConverter using templates

--- src/util/ContentTypes.ts
+++ src/util/ContentTypes.ts
@@ -1,9 +1,10 @@
 // Well-known content types
-export const TEXT_TURTLE = 'text/turtle';
 export const APPLICATION_JSON = 'application/json';
 export const APPLICATION_OCTET_STREAM = 'application/octet-stream';
 export const APPLICATION_SPARQL_UPDATE = 'application/sparql-update';
 export const APPLICATION_X_WWW_FORM_URLENCODED = 'application/x-www-form-urlencoded';
+export const TEXT_HTML = 'text/html';
+export const TEXT_TURTLE = 'text/turtle';
 
 // Internal content types (not exposed over HTTP)
 export const INTERNAL_ALL = 'internal/*';
