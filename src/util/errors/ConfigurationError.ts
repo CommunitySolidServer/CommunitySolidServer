3e9f294123eb660f4572ee64703594da2488e957
Joachim Van Herwegen <joachimvh@gmail.com>
2021-06-22 11:13:50 +0200

    refactor: Remove unused ConfigurationError

--- src/util/errors/ConfigurationError.ts
+++
@@ -1,8 +0,0 @@
-/**
- * An error thrown when something is flawed about the configuration.
- */
-export class ConfigurationError extends Error {
-  public constructor(message: string) {
-    super(message);
-  }
-}
