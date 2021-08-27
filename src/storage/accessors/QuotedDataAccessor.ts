266e200659da343b94bb294054177da3410d135b
Arthur Joppart <arthur@digita.ai>
2021-08-27 13:57:48 +0200

    feat: added QuotedDataAccessor

---
+++ src/storage/accessors/QuotedDataAccessor.ts
@@ -0,0 +1,59 @@
+import type { Readable } from 'stream';
+import type { QuotaStrategy } from 'somewhere';
+import type { Representation } from '../../ldp/representation/Representation';
+import type { RepresentationMetadata } from '../../ldp/representation/RepresentationMetadata';
+import type { ResourceIdentifier } from '../../ldp/representation/ResourceIdentifier';
+import type { Guarded } from '../../util/GuardedStream';
+import type { DataAccessor } from './DataAccessor';
+
+export class QuotedDataAccessor implements DataAccessor {
+  private readonly accessor: DataAccessor;
+  private readonly strategy: QuotaStrategy;
+
+  public constructor(accessor: DataAccessor, strategy: QuotaStrategy) {
+    this.accessor = accessor;
+    this.strategy = strategy;
+  }
+
+  // Size of the metadata is not taken into account yet, i think
+
+  public async writeDocument(
+    identifier: ResourceIdentifier,
+    data: Guarded<Readable>,
+    metadata: RepresentationMetadata,
+  ): Promise<void> {
+    // Use this value in the return call:
+    // const pipedData = this.strategy.limitStream(identifier, data, metadata);
+    return this.accessor.writeDocument(identifier, data, metadata);
+  }
+
+  public async writeContainer(
+    identifier: ResourceIdentifier,
+    metadata: RepresentationMetadata,
+  ): Promise<void> {
+    // Some kind of checking should be done here as well
+    return this.accessor.writeContainer(identifier, metadata);
+  }
+
+  // The following functions are unchanged and simply get passed along to the accessor
+
+  public async canHandle(representation: Representation): Promise<void> {
+    return this.accessor.canHandle(representation);
+  }
+
+  public async getData(identifier: ResourceIdentifier): Promise<Guarded<Readable>> {
+    return this.accessor.getData(identifier);
+  }
+
+  public async getMetadata(identifier: ResourceIdentifier): Promise<RepresentationMetadata> {
+    return this.accessor.getMetadata(identifier);
+  }
+
+  public getChildren(identifier: ResourceIdentifier): AsyncIterableIterator<RepresentationMetadata> {
+    return this.accessor.getChildren(identifier);
+  }
+
+  public async deleteResource(identifier: ResourceIdentifier): Promise<void> {
+    return this.accessor.deleteResource(identifier);
+  }
+}
