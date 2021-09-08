joeitu/community-server

feat: Support JSON errors
The IDP behaviour has been changed to move all error related knowledge
to the IdentityProviderHttpHandler instead of managing it
in the Interactionhandlers.

src/identity/interaction/email-password/handler/ForgotPasswordHandler.ts
@@ -4,7 +4,6 @@ import { ensureTrailingSlash, joinUrl } from '../../../../util/PathUtil';
 import { readableToDict } from '../../../../util/StreamUtil';
 import type { TemplateEngine } from '../../../../util/templates/TemplateEngine';
 import type { EmailSender } from '../../util/EmailSender';
-import { throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import { InteractionHandler } from './InteractionHandler';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
@@ -39,16 +38,12 @@ export class ForgotPasswordHandler extends InteractionHandler {
   }
 
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionResponseResult<{ email: string }>> {
-    try {
-      // Validate incoming data
-      const { email } = await readableToDict(operation.body!.data);
-      assert(typeof email === 'string' && email.length > 0, 'Email required');
+    // Validate incoming data
+    const { email } = await readableToDict(operation.body!.data);
+    assert(typeof email === 'string' && email.length > 0, 'Email required');
 
-      await this.resetPassword(email);
-      return { type: 'response', details: { email }};
-    } catch (err: unknown) {
-      throwIdpInteractionError(err, {});
-    }
+    await this.resetPassword(email);
+    return { type: 'response', details: { email }};
   }
 
   /**

src/identity/interaction/email-password/handler/LoginHandler.ts
@@ -2,7 +2,6 @@ import assert from 'assert';
 import type { Operation } from '../../../../ldp/operations/Operation';
 import { getLoggerFor } from '../../../../logging/LogUtil';
 import { readableToDict } from '../../../../util/StreamUtil';
-import { throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import { InteractionHandler } from './InteractionHandler';
 import type { InteractionCompleteResult, InteractionHandlerInput } from './InteractionHandler';
@@ -22,34 +21,26 @@ export class LoginHandler extends InteractionHandler {
 
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionCompleteResult> {
     const { email, password, remember } = await this.parseInput(operation);
-    try {
-      // Try to log in, will error if email/password combination is invalid
-      const webId = await this.accountStore.authenticate(email, password);
-      this.logger.debug(`Logging in user ${email}`);
-      return {
-        type: 'complete',
-        details: { webId, shouldRemember: Boolean(remember) },
-      };
-    } catch (err: unknown) {
-      throwIdpInteractionError(err, { email });
-    }
+    // Try to log in, will error if email/password combination is invalid
+    const webId = await this.accountStore.authenticate(email, password);
+    this.logger.debug(`Logging in user ${email}`);
+    return {
+      type: 'complete',
+      details: { webId, shouldRemember: Boolean(remember) },
+    };
   }
 
   /**
    * Parses and validates the input form data.
-   * Will throw an {@link IdpInteractionError} in case something is wrong.
+   * Will throw an error in case something is wrong.
    * All relevant data that was correct up to that point will be prefilled.
    */
   private async parseInput(operation: Operation): Promise<{ email: string; password: string; remember: boolean }> {
     const prefilled: Record<string, string> = {};
-    try {
-      const { email, password, remember } = await readableToDict(operation.body!.data);
-      assert(typeof email === 'string' && email.length > 0, 'Email required');
-      prefilled.email = email;
-      assert(typeof password === 'string' && password.length > 0, 'Password required');
-      return { email, password, remember: Boolean(remember) };
-    } catch (err: unknown) {
-      throwIdpInteractionError(err, prefilled);
-    }
+    const { email, password, remember } = await readableToDict(operation.body!.data);
+    assert(typeof email === 'string' && email.length > 0, 'Email required');
+    prefilled.email = email;
+    assert(typeof password === 'string' && password.length > 0, 'Password required');
+    return { email, password, remember: Boolean(remember) };
   }
 }

src/identity/interaction/email-password/handler/ResetPasswordHandler.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { getLoggerFor } from '../../../../logging/LogUtil';
 import { readableToDict } from '../../../../util/StreamUtil';
-import { assertPassword, throwIdpInteractionError } from '../EmailPasswordUtil';
+import { assertPassword } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
 import { InteractionHandler } from './InteractionHandler';
@@ -21,22 +21,18 @@ export class ResetPasswordHandler extends InteractionHandler {
   }
 
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionResponseResult> {
-    try {
-      // Extract record ID from request URL
-      const recordId = /\/([^/]+)$/u.exec(operation.target.path)?.[1];
-      // Validate input data
-      const { password, confirmPassword } = await readableToDict(operation.body!.data);
-      assert(
-        typeof recordId === 'string' && recordId.length > 0,
-        'Invalid request. Open the link from your email again',
-      );
-      assertPassword(password, confirmPassword);
+    // Extract record ID from request URL
+    const recordId = /\/([^/]+)$/u.exec(operation.target.path)?.[1];
+    // Validate input data
+    const { password, confirmPassword } = await readableToDict(operation.body!.data);
+    assert(
+      typeof recordId === 'string' && recordId.length > 0,
+      'Invalid request. Open the link from your email again',
+    );
+    assertPassword(password, confirmPassword);
 
-      await this.resetPassword(recordId, password);
-      return { type: 'response' };
-    } catch (error: unknown) {
-      throwIdpInteractionError(error);
-    }
+    await this.resetPassword(recordId, password);
+    return { type: 'response' };
   }
 
   /**

test/unit/storage/conversion/ErrorToJsonConverter.test.ts
@@ -1,0 +1,107 @@
+import { BasicRepresentation } from '../../../../src/ldp/representation/BasicRepresentation';
+import { ErrorToJsonConverter } from '../../../../src/storage/conversion/ErrorToJsonConverter';
+import { BadRequestHttpError } from '../../../../src/util/errors/BadRequestHttpError';
+import { InternalServerError } from '../../../../src/util/errors/InternalServerError';
+import { readableToDict } from '../../../../src/util/StreamUtil';
+
+describe('An ErrorToJsonConverter', (): void => {
+  const identifier = { path: 'http://test.com/error' };
+  const converter = new ErrorToJsonConverter();
+  const preferences = {};
+
+  it('supports going from errors to json.', async(): Promise<void> => {
+    await expect(converter.getInputTypes()).resolves.toEqual({ 'internal/error': 1 });
+    await expect(converter.getOutputTypes()).resolves.toEqual({ 'application/json': 1 });
+  });
+
+  it('does not support multiple errors.', async(): Promise<void> => {
+    const representation = new BasicRepresentation([ new Error('a'), new Error('b') ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).rejects.toThrow('Only single errors are supported.');
+    await expect(prom).rejects.toThrow(InternalServerError);
+  });
+
+  it('adds all HttpError fields.', async(): Promise<void> => {
+    const error = new BadRequestHttpError('error text');
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readableToDict(result.data)).resolves.toEqual({
+      name: 'BadRequestHttpError',
+      message: 'error text',
+      statusCode: 400,
+      errorCode: 'H400',
+      stack: error.stack,
+    });
+  });
+
+  it('copies the HttpError details.', async(): Promise<void> => {
+    const error = new BadRequestHttpError('error text', { details: { important: 'detail' }});
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readableToDict(result.data)).resolves.toEqual({
+      name: 'BadRequestHttpError',
+      message: 'error text',
+      statusCode: 400,
+      errorCode: 'H400',
+      details: { important: 'detail' },
+      stack: error.stack,
+    });
+  });
+
+  it('does not copy the details if they are not serializable.', async(): Promise<void> => {
+    const error = new BadRequestHttpError('error text', { details: { object: BigInt(1) }});
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readableToDict(result.data)).resolves.toEqual({
+      name: 'BadRequestHttpError',
+      message: 'error text',
+      statusCode: 400,
+      errorCode: 'H400',
+      stack: error.stack,
+    });
+  });
+
+  it('defaults to status code 500 for non-HTTP errors.', async(): Promise<void> => {
+    const error = new Error('error text');
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readableToDict(result.data)).resolves.toEqual({
+      name: 'Error',
+      message: 'error text',
+      statusCode: 500,
+      stack: error.stack,
+    });
+  });
+
+  it('only adds stack if it is defined.', async(): Promise<void> => {
+    const error = new Error('error text');
+    delete error.stack;
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readableToDict(result.data)).resolves.toEqual({
+      name: 'Error',
+      message: 'error text',
+      statusCode: 500,
+    });
+  });
+});

CommunitySolidServer/CommunitySolidServer

src/identity/interaction/email-password/handler/ForgotPasswordHandler.ts
@@ -4,7 +4,6 @@ import { ensureTrailingSlash, joinUrl } from '../../../../util/PathUtil';
 import { readJsonStream } from '../../../../util/StreamUtil';
 import type { TemplateEngine } from '../../../../util/templates/TemplateEngine';
 import type { EmailSender } from '../../util/EmailSender';
-import { throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import { InteractionHandler } from './InteractionHandler';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
@@ -39,16 +38,12 @@ export class ForgotPasswordHandler extends InteractionHandler {
   }
 
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionResponseResult<{ email: string }>> {
-    try {
-      // Validate incoming data
-      const { email } = await readJsonStream(operation.body!.data);
-      assert(typeof email === 'string' && email.length > 0, 'Email required');
+    // Validate incoming data
+    const { email } = await readJsonStream(operation.body!.data);
+    assert(typeof email === 'string' && email.length > 0, 'Email required');
 
-      await this.resetPassword(email);
-      return { type: 'response', details: { email }};
-    } catch (err: unknown) {
-      throwIdpInteractionError(err, {});
-    }
+    await this.resetPassword(email);
+    return { type: 'response', details: { email }};
   }
 
   /**

src/identity/interaction/email-password/handler/LoginHandler.ts
@@ -2,7 +2,6 @@ import assert from 'assert';
 import type { Operation } from '../../../../ldp/operations/Operation';
 import { getLoggerFor } from '../../../../logging/LogUtil';
 import { readJsonStream } from '../../../../util/StreamUtil';
-import { throwIdpInteractionError } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import { InteractionHandler } from './InteractionHandler';
 import type { InteractionCompleteResult, InteractionHandlerInput } from './InteractionHandler';
@@ -22,34 +21,26 @@ export class LoginHandler extends InteractionHandler {
 
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionCompleteResult> {
     const { email, password, remember } = await this.parseInput(operation);
-    try {
-      // Try to log in, will error if email/password combination is invalid
-      const webId = await this.accountStore.authenticate(email, password);
-      this.logger.debug(`Logging in user ${email}`);
-      return {
-        type: 'complete',
-        details: { webId, shouldRemember: remember },
-      };
-    } catch (err: unknown) {
-      throwIdpInteractionError(err, { email });
-    }
+    // Try to log in, will error if email/password combination is invalid
+    const webId = await this.accountStore.authenticate(email, password);
+    this.logger.debug(`Logging in user ${email}`);
+    return {
+      type: 'complete',
+      details: { webId, shouldRemember: remember },
+    };
   }
 
   /**
    * Parses and validates the input form data.
-   * Will throw an {@link IdpInteractionError} in case something is wrong.
+   * Will throw an error in case something is wrong.
    * All relevant data that was correct up to that point will be prefilled.
    */
   private async parseInput(operation: Operation): Promise<{ email: string; password: string; remember: boolean }> {
     const prefilled: Record<string, string> = {};
-    try {
-      const { email, password, remember } = await readJsonStream(operation.body!.data);
-      assert(typeof email === 'string' && email.length > 0, 'Email required');
-      prefilled.email = email;
-      assert(typeof password === 'string' && password.length > 0, 'Password required');
-      return { email, password, remember: Boolean(remember) };
-    } catch (err: unknown) {
-      throwIdpInteractionError(err, prefilled);
-    }
+    const { email, password, remember } = await readJsonStream(operation.body!.data);
+    assert(typeof email === 'string' && email.length > 0, 'Email required');
+    prefilled.email = email;
+    assert(typeof password === 'string' && password.length > 0, 'Password required');
+    return { email, password, remember: Boolean(remember) };
   }
 }

src/identity/interaction/email-password/handler/ResetPasswordHandler.ts
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { getLoggerFor } from '../../../../logging/LogUtil';
 import { readJsonStream } from '../../../../util/StreamUtil';
-import { assertPassword, throwIdpInteractionError } from '../EmailPasswordUtil';
+import { assertPassword } from '../EmailPasswordUtil';
 import type { AccountStore } from '../storage/AccountStore';
 import type { InteractionResponseResult, InteractionHandlerInput } from './InteractionHandler';
 import { InteractionHandler } from './InteractionHandler';
@@ -21,22 +21,18 @@ export class ResetPasswordHandler extends InteractionHandler {
   }
 
   public async handle({ operation }: InteractionHandlerInput): Promise<InteractionResponseResult> {
-    try {
-      // Extract record ID from request URL
-      const recordId = /\/([^/]+)$/u.exec(operation.target.path)?.[1];
-      // Validate input data
-      const { password, confirmPassword } = await readJsonStream(operation.body!.data);
-      assert(
-        typeof recordId === 'string' && recordId.length > 0,
-        'Invalid request. Open the link from your email again',
-      );
-      assertPassword(password, confirmPassword);
+    // Extract record ID from request URL
+    const recordId = /\/([^/]+)$/u.exec(operation.target.path)?.[1];
+    // Validate input data
+    const { password, confirmPassword } = await readJsonStream(operation.body!.data);
+    assert(
+      typeof recordId === 'string' && recordId.length > 0,
+      'Invalid request. Open the link from your email again',
+    );
+    assertPassword(password, confirmPassword);
 
-      await this.resetPassword(recordId, password);
-      return { type: 'response' };
-    } catch (error: unknown) {
-      throwIdpInteractionError(error);
-    }
+    await this.resetPassword(recordId, password);
+    return { type: 'response' };
   }
 
   /**

test/unit/storage/conversion/ErrorToJsonConverter.test.ts
@@ -1,0 +1,99 @@
+import { BasicRepresentation } from '../../../../src/ldp/representation/BasicRepresentation';
+import { ErrorToJsonConverter } from '../../../../src/storage/conversion/ErrorToJsonConverter';
+import { BadRequestHttpError } from '../../../../src/util/errors/BadRequestHttpError';
+import { readJsonStream } from '../../../../src/util/StreamUtil';
+
+describe('An ErrorToJsonConverter', (): void => {
+  const identifier = { path: 'http://test.com/error' };
+  const converter = new ErrorToJsonConverter();
+  const preferences = {};
+
+  it('supports going from errors to json.', async(): Promise<void> => {
+    await expect(converter.getInputTypes()).resolves.toEqual({ 'internal/error': 1 });
+    await expect(converter.getOutputTypes()).resolves.toEqual({ 'application/json': 1 });
+  });
+
+  it('adds all HttpError fields.', async(): Promise<void> => {
+    const error = new BadRequestHttpError('error text');
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readJsonStream(result.data)).resolves.toEqual({
+      name: 'BadRequestHttpError',
+      message: 'error text',
+      statusCode: 400,
+      errorCode: 'H400',
+      stack: error.stack,
+    });
+  });
+
+  it('copies the HttpError details.', async(): Promise<void> => {
+    const error = new BadRequestHttpError('error text', { details: { important: 'detail' }});
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readJsonStream(result.data)).resolves.toEqual({
+      name: 'BadRequestHttpError',
+      message: 'error text',
+      statusCode: 400,
+      errorCode: 'H400',
+      details: { important: 'detail' },
+      stack: error.stack,
+    });
+  });
+
+  it('does not copy the details if they are not serializable.', async(): Promise<void> => {
+    const error = new BadRequestHttpError('error text', { details: { object: BigInt(1) }});
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readJsonStream(result.data)).resolves.toEqual({
+      name: 'BadRequestHttpError',
+      message: 'error text',
+      statusCode: 400,
+      errorCode: 'H400',
+      stack: error.stack,
+    });
+  });
+
+  it('defaults to status code 500 for non-HTTP errors.', async(): Promise<void> => {
+    const error = new Error('error text');
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readJsonStream(result.data)).resolves.toEqual({
+      name: 'Error',
+      message: 'error text',
+      statusCode: 500,
+      stack: error.stack,
+    });
+  });
+
+  it('only adds stack if it is defined.', async(): Promise<void> => {
+    const error = new Error('error text');
+    delete error.stack;
+    const representation = new BasicRepresentation([ error ], 'internal/error', false);
+    const prom = converter.handle({ identifier, representation, preferences });
+    await expect(prom).resolves.toBeDefined();
+    const result = await prom;
+    expect(result.binary).toBe(true);
+    expect(result.metadata.contentType).toBe('application/json');
+    await expect(readJsonStream(result.data)).resolves.toEqual({
+      name: 'Error',
+      message: 'error text',
+      statusCode: 500,
+    });
+  });
+});