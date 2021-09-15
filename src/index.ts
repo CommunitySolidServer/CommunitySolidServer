facf691e867a1e952483c32f4ac3bf824418fa57
Joachim Van Herwegen <joachimvh@gmail.com>
2021-09-15 16:47:39 +0200

    feat: Create ConditionalHandler
    This handler rejects all inputs once a certain condition is met

--- src/index.ts
+++ src/index.ts
@@ -307,6 +307,7 @@ export * from './util/errors/UnsupportedMediaTypeHttpError';
 // Util/Handlers
 export * from './util/handlers/AsyncHandler';
 export * from './util/handlers/BooleanHandler';
+export * from './util/handlers/ConditionalHandler';
 export * from './util/handlers/ParallelHandler';
 export * from './util/handlers/SequenceHandler';
 export * from './util/handlers/UnsupportedAsyncHandler';
