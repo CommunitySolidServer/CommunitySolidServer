rpatil524/community-server

feat: Use PermissionReaders to determine available permissions
These readers will determine which permissions
are available for the incoming credentials.
Their results then get combined in a UnionReader
and authorized in a PermissionBasedAuthorizer

src/index.ts
@@ -10,14 +10,16 @@ export * from './authentication/UnsecureConstantCredentialsExtractor';
 export * from './authentication/UnsecureWebIdExtractor';
 
 // Authorization
-export * from './authorization/AllowAllAuthorizer';
+export * from './authorization/AllStaticReader';
 export * from './authorization/Authorization';
 export * from './authorization/Authorizer';
-export * from './authorization/AuxiliaryAuthorizer';
-export * from './authorization/DenyAllAuthorizer';
-export * from './authorization/PathBasedAuthorizer';
+export * from './authorization/AuxiliaryReader';
+export * from './authorization/PathBasedReader';
+export * from './authorization/PermissionBasedAuthorizer';
+export * from './authorization/PermissionReader';
+export * from './authorization/UnionReader';
 export * from './authorization/WebAclAuthorization';
-export * from './authorization/WebAclAuthorizer';
+export * from './authorization/WebAclReader';
 
 // Authorization/access-checkers
 export * from './authorization/access-checkers/AccessChecker';
@@ -146,7 +148,7 @@ export * from './ldp/operations/PutOperationHandler';
 
 // LDP/Permissions
 export * from './ldp/permissions/AclPermissionsExtractor';
-export * from './ldp/permissions/PermissionSet';
+export * from './ldp/permissions/Permissions';
 export * from './ldp/permissions/PermissionsExtractor';
 export * from './ldp/permissions/MethodPermissionsExtractor';
 export * from './ldp/permissions/SparqlPatchPermissionsExtractor';

src/ldp/permissions/Permissions.ts
@@ -1,0 +1,14 @@
+import type { ALL_CREDENTIALS } from '../../authentication/CredentialTypes';
+
+/**
+ * A data interface indicating which permissions are required (based on the context).
+ */
+export interface Permissions {
+  read?: boolean;
+  append?: boolean;
+  write?: boolean;
+  control?: boolean;
+}
+
+export type PermissionSet = Partial<Record<ALL_CREDENTIALS, Permissions>>;
+

test/integration/LdpHandlerWithAuth.test.ts
@@ -89,7 +89,7 @@ describe.each(stores)('An LDP handler with auth using %s', (name, { storeConfig,
     // GET
     const response = await getResource(document);
     await expect(response.text()).resolves.toBe('TESTDATA');
-    expect(response.headers.get('wac-allow')).toBe('user="read write append",public="read write append"');
+    expect(response.headers.get('wac-allow')).toBe('user="read append write",public="read append write"');
 
     // DELETE
     await deleteResource(document);

test/unit/authorization/AuxiliaryReader.test.ts
@@ -1,32 +1,26 @@
-import type { Authorizer } from '../../../src/authorization/Authorizer';
-import { AuxiliaryAuthorizer } from '../../../src/authorization/AuxiliaryAuthorizer';
+import { AGENT } from '../../../src/authentication/CredentialTypes';
+import { AuxiliaryReader } from '../../../src/authorization/AuxiliaryReader';
+import type { PermissionReader } from '../../../src/authorization/PermissionReader';
 import type { AuxiliaryIdentifierStrategy } from '../../../src/ldp/auxiliary/AuxiliaryIdentifierStrategy';
-import type { PermissionSet } from '../../../src/ldp/permissions/PermissionSet';
+import type { PermissionSet } from '../../../src/ldp/permissions/Permissions';
 import type { ResourceIdentifier } from '../../../src/ldp/representation/ResourceIdentifier';
 import { NotImplementedHttpError } from '../../../src/util/errors/NotImplementedHttpError';
 
-describe('An AuxiliaryAuthorizer', (): void => {
+describe('An AuxiliaryReader', (): void => {
   const suffix = '.dummy';
   const credentials = {};
   const associatedIdentifier = { path: 'http://test.com/foo' };
   const auxiliaryIdentifier = { path: 'http://test.com/foo.dummy' };
-  let permissions: PermissionSet;
-  let source: Authorizer;
+  const permissionSet: PermissionSet = { [AGENT]: { read: true }};
+  let source: PermissionReader;
   let strategy: AuxiliaryIdentifierStrategy;
-  let authorizer: AuxiliaryAuthorizer;
+  let reader: AuxiliaryReader;
 
   beforeEach(async(): Promise<void> => {
-    permissions = {
-      read: true,
-      write: true,
-      append: true,
-      control: false,
-    };
-
     source = {
       canHandle: jest.fn(),
-      handle: jest.fn(),
-      handleSafe: jest.fn(),
+      handle: jest.fn().mockResolvedValue(permissionSet),
+      handleSafe: jest.fn().mockResolvedValue(permissionSet),
     };
 
     strategy = {
@@ -34,43 +28,43 @@ describe('An AuxiliaryAuthorizer', (): void => {
       getAssociatedIdentifier: jest.fn((identifier: ResourceIdentifier): ResourceIdentifier =>
         ({ path: identifier.path.slice(0, -suffix.length) })),
     } as any;
-    authorizer = new AuxiliaryAuthorizer(source, strategy);
+    reader = new AuxiliaryReader(source, strategy);
   });
 
   it('can handle auxiliary resources if the source supports the associated resource.', async(): Promise<void> => {
-    await expect(authorizer.canHandle({ identifier: auxiliaryIdentifier, credentials, permissions }))
+    await expect(reader.canHandle({ identifier: auxiliaryIdentifier, credentials }))
       .resolves.toBeUndefined();
     expect(source.canHandle).toHaveBeenLastCalledWith(
-      { identifier: associatedIdentifier, credentials, permissions },
+      { identifier: associatedIdentifier, credentials },
     );
-    await expect(authorizer.canHandle({ identifier: associatedIdentifier, credentials, permissions }))
+    await expect(reader.canHandle({ identifier: associatedIdentifier, credentials }))
       .rejects.toThrow(NotImplementedHttpError);
     source.canHandle = jest.fn().mockRejectedValue(new Error('no source support'));
-    await expect(authorizer.canHandle({ identifier: auxiliaryIdentifier, credentials, permissions }))
+    await expect(reader.canHandle({ identifier: auxiliaryIdentifier, credentials }))
       .rejects.toThrow('no source support');
   });
 
   it('handles resources by sending the updated parameters to the source.', async(): Promise<void> => {
-    await expect(authorizer.handle({ identifier: auxiliaryIdentifier, credentials, permissions }))
-      .resolves.toBeUndefined();
+    await expect(reader.handle({ identifier: auxiliaryIdentifier, credentials }))
+      .resolves.toBe(permissionSet);
     expect(source.handle).toHaveBeenLastCalledWith(
-      { identifier: associatedIdentifier, credentials, permissions },
+      { identifier: associatedIdentifier, credentials },
     );
     // Safety checks are not present when calling `handle`
-    await expect(authorizer.handle({ identifier: associatedIdentifier, credentials, permissions }))
+    await expect(reader.handle({ identifier: associatedIdentifier, credentials }))
       .rejects.toThrow(NotImplementedHttpError);
   });
 
   it('combines both checking and handling when calling handleSafe.', async(): Promise<void> => {
-    await expect(authorizer.handleSafe({ identifier: auxiliaryIdentifier, credentials, permissions }))
-      .resolves.toBeUndefined();
+    await expect(reader.handleSafe({ identifier: auxiliaryIdentifier, credentials }))
+      .resolves.toBe(permissionSet);
     expect(source.handleSafe).toHaveBeenLastCalledWith(
-      { identifier: associatedIdentifier, credentials, permissions },
+      { identifier: associatedIdentifier, credentials },
     );
-    await expect(authorizer.handleSafe({ identifier: associatedIdentifier, credentials, permissions }))
+    await expect(reader.handleSafe({ identifier: associatedIdentifier, credentials }))
       .rejects.toThrow(NotImplementedHttpError);
     source.handleSafe = jest.fn().mockRejectedValue(new Error('no source support'));
-    await expect(authorizer.handleSafe({ identifier: auxiliaryIdentifier, credentials, permissions }))
+    await expect(reader.handleSafe({ identifier: auxiliaryIdentifier, credentials }))
       .rejects.toThrow('no source support');
   });
 });

test/util/AclHelper.ts
@@ -1,4 +1,4 @@
-import type { ResourceStore, PermissionSet } from '../../src/';
+import type { ResourceStore, Permissions } from '../../src/';
 import { BasicRepresentation } from '../../src/';
 
 export class AclHelper {
@@ -11,7 +11,7 @@ export class AclHelper {
   public async setSimpleAcl(
     resource: string,
     options: {
-      permissions: Partial<PermissionSet>;
+      permissions: Partial<Permissions>;
       agentClass?: 'agent' | 'authenticated';
       agent?: string;
       accessTo?: boolean;
@@ -32,7 +32,7 @@ export class AclHelper {
     ];
 
     for (const perm of [ 'Read', 'Append', 'Write', 'Control' ]) {
-      if (options.permissions[perm.toLowerCase() as keyof PermissionSet]) {
+      if (options.permissions[perm.toLowerCase() as keyof Permissions]) {
         acl.push(`;\n acl:mode acl:${perm}`);
       }
     }

CommunitySolidServer/CommunitySolidServer

src/index.ts
@@ -9,14 +9,16 @@ export * from './authentication/UnsecureConstantCredentialsExtractor';
 export * from './authentication/UnsecureWebIdExtractor';
 
 // Authorization
-export * from './authorization/AllowAllAuthorizer';
+export * from './authorization/AllStaticReader';
 export * from './authorization/Authorization';
 export * from './authorization/Authorizer';
-export * from './authorization/AuxiliaryAuthorizer';
-export * from './authorization/DenyAllAuthorizer';
-export * from './authorization/PathBasedAuthorizer';
+export * from './authorization/AuxiliaryReader';
+export * from './authorization/PathBasedReader';
+export * from './authorization/PermissionBasedAuthorizer';
+export * from './authorization/PermissionReader';
+export * from './authorization/UnionPermissionReader';
 export * from './authorization/WebAclAuthorization';
-export * from './authorization/WebAclAuthorizer';
+export * from './authorization/WebAclReader';
 
 // Authorization/access-checkers
 export * from './authorization/access-checkers/AccessChecker';
@@ -151,7 +153,7 @@ export * from './ldp/operations/PutOperationHandler';
 
 // LDP/Permissions
 export * from './ldp/permissions/AclModesExtractor';
-export * from './ldp/permissions/PermissionSet';
+export * from './ldp/permissions/Permissions';
 export * from './ldp/permissions/ModesExtractor';
 export * from './ldp/permissions/MethodModesExtractor';
 export * from './ldp/permissions/SparqlPatchModesExtractor';

src/ldp/permissions/Permissions.ts
@@ -1,3 +1,5 @@
+import type { CredentialGroup } from '../../authentication/Credentials';
+
 /**
  * Different modes that require permission.
  */
@@ -11,4 +13,6 @@ export enum AccessMode {
 /**
  * A data interface indicating which permissions are required (based on the context).
  */
-export type PermissionSet = Record<AccessMode, boolean>;
+export type Permission = Partial<Record<AccessMode, boolean>>;
+
+export type PermissionSet = Partial<Record<CredentialGroup, Permission>>;

test/integration/LdpHandlerWithAuth.test.ts
@@ -89,7 +89,7 @@ describe.each(stores)('An LDP handler with auth using %s', (name, { storeConfig,
     // GET
     const response = await getResource(document);
     await expect(response.text()).resolves.toBe('TESTDATA');
-    expect(response.headers.get('wac-allow')).toBe('user="read write append",public="read write append"');
+    expect(response.headers.get('wac-allow')).toBe('user="append read write",public="append read write"');
 
     // DELETE
     await deleteResource(document);

test/unit/authorization/AuxiliaryReader.test.ts
@@ -1,27 +1,26 @@
-import type { Authorizer } from '../../../src/authorization/Authorizer';
-import { AuxiliaryAuthorizer } from '../../../src/authorization/AuxiliaryAuthorizer';
+import { CredentialGroup } from '../../../src/authentication/Credentials';
+import { AuxiliaryReader } from '../../../src/authorization/AuxiliaryReader';
+import type { PermissionReader } from '../../../src/authorization/PermissionReader';
 import type { AuxiliaryIdentifierStrategy } from '../../../src/ldp/auxiliary/AuxiliaryIdentifierStrategy';
-import { AccessMode } from '../../../src/ldp/permissions/PermissionSet';
+import type { PermissionSet } from '../../../src/ldp/permissions/Permissions';
 import type { ResourceIdentifier } from '../../../src/ldp/representation/ResourceIdentifier';
 import { NotImplementedHttpError } from '../../../src/util/errors/NotImplementedHttpError';
 
-describe('An AuxiliaryAuthorizer', (): void => {
+describe('An AuxiliaryReader', (): void => {
   const suffix = '.dummy';
   const credentials = {};
   const associatedIdentifier = { path: 'http://test.com/foo' };
   const auxiliaryIdentifier = { path: 'http://test.com/foo.dummy' };
-  let modes: Set<AccessMode>;
-  let source: Authorizer;
+  const permissionSet: PermissionSet = { [CredentialGroup.agent]: { read: true }};
+  let source: PermissionReader;
   let strategy: AuxiliaryIdentifierStrategy;
-  let authorizer: AuxiliaryAuthorizer;
+  let reader: AuxiliaryReader;
 
   beforeEach(async(): Promise<void> => {
-    modes = new Set([ AccessMode.read, AccessMode.write, AccessMode.append ]);
-
     source = {
       canHandle: jest.fn(),
-      handle: jest.fn(),
-      handleSafe: jest.fn(),
+      handle: jest.fn().mockResolvedValue(permissionSet),
+      handleSafe: jest.fn().mockResolvedValue(permissionSet),
     };
 
     strategy = {
@@ -29,43 +28,43 @@ describe('An AuxiliaryAuthorizer', (): void => {
       getAssociatedIdentifier: jest.fn((identifier: ResourceIdentifier): ResourceIdentifier =>
         ({ path: identifier.path.slice(0, -suffix.length) })),
     } as any;
-    authorizer = new AuxiliaryAuthorizer(source, strategy);
+    reader = new AuxiliaryReader(source, strategy);
   });
 
   it('can handle auxiliary resources if the source supports the associated resource.', async(): Promise<void> => {
-    await expect(authorizer.canHandle({ identifier: auxiliaryIdentifier, credentials, modes }))
+    await expect(reader.canHandle({ identifier: auxiliaryIdentifier, credentials }))
       .resolves.toBeUndefined();
     expect(source.canHandle).toHaveBeenLastCalledWith(
-      { identifier: associatedIdentifier, credentials, modes },
+      { identifier: associatedIdentifier, credentials },
     );
-    await expect(authorizer.canHandle({ identifier: associatedIdentifier, credentials, modes }))
+    await expect(reader.canHandle({ identifier: associatedIdentifier, credentials }))
       .rejects.toThrow(NotImplementedHttpError);
     source.canHandle = jest.fn().mockRejectedValue(new Error('no source support'));
-    await expect(authorizer.canHandle({ identifier: auxiliaryIdentifier, credentials, modes }))
+    await expect(reader.canHandle({ identifier: auxiliaryIdentifier, credentials }))
       .rejects.toThrow('no source support');
   });
 
   it('handles resources by sending the updated parameters to the source.', async(): Promise<void> => {
-    await expect(authorizer.handle({ identifier: auxiliaryIdentifier, credentials, modes }))
-      .resolves.toBeUndefined();
+    await expect(reader.handle({ identifier: auxiliaryIdentifier, credentials }))
+      .resolves.toBe(permissionSet);
     expect(source.handle).toHaveBeenLastCalledWith(
-      { identifier: associatedIdentifier, credentials, modes },
+      { identifier: associatedIdentifier, credentials },
     );
     // Safety checks are not present when calling `handle`
-    await expect(authorizer.handle({ identifier: associatedIdentifier, credentials, modes }))
+    await expect(reader.handle({ identifier: associatedIdentifier, credentials }))
       .rejects.toThrow(NotImplementedHttpError);
   });
 
   it('combines both checking and handling when calling handleSafe.', async(): Promise<void> => {
-    await expect(authorizer.handleSafe({ identifier: auxiliaryIdentifier, credentials, modes }))
-      .resolves.toBeUndefined();
+    await expect(reader.handleSafe({ identifier: auxiliaryIdentifier, credentials }))
+      .resolves.toBe(permissionSet);
     expect(source.handleSafe).toHaveBeenLastCalledWith(
-      { identifier: associatedIdentifier, credentials, modes },
+      { identifier: associatedIdentifier, credentials },
     );
-    await expect(authorizer.handleSafe({ identifier: associatedIdentifier, credentials, modes }))
+    await expect(reader.handleSafe({ identifier: associatedIdentifier, credentials }))
       .rejects.toThrow(NotImplementedHttpError);
     source.handleSafe = jest.fn().mockRejectedValue(new Error('no source support'));
-    await expect(authorizer.handleSafe({ identifier: auxiliaryIdentifier, credentials, modes }))
+    await expect(reader.handleSafe({ identifier: auxiliaryIdentifier, credentials }))
       .rejects.toThrow('no source support');
   });
 });

test/util/AclHelper.ts
@@ -1,4 +1,4 @@
-import type { ResourceStore, PermissionSet } from '../../src/';
+import type { ResourceStore, Permission } from '../../src/';
 import { BasicRepresentation } from '../../src/';
 
 export class AclHelper {
@@ -11,7 +11,7 @@ export class AclHelper {
   public async setSimpleAcl(
     resource: string,
     options: {
-      permissions: Partial<PermissionSet>;
+      permissions: Partial<Permission>;
       agentClass?: 'agent' | 'authenticated';
       agent?: string;
       accessTo?: boolean;
@@ -32,7 +32,7 @@ export class AclHelper {
     ];
 
     for (const perm of [ 'Read', 'Append', 'Write', 'Control' ]) {
-      if (options.permissions[perm.toLowerCase() as keyof PermissionSet]) {
+      if (options.permissions[perm.toLowerCase() as keyof Permission]) {
         acl.push(`;\n acl:mode acl:${perm}`);
       }
     }