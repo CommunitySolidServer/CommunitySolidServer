79f7e7a4c8ff2cc5820e1bc4741a46ebd3d37a3b
Arthur Joppart <arthur@digita.ai>
2021-10-12 14:07:32 +0200

    test: GlobalQuotaStrategy tests

---
+++ test/unit/quota/GlobalQuotaStrategy.test.ts
@@ -0,0 +1,99 @@
+import { RepresentationMetadata } from '../../../src/ldp/representation/RepresentationMetadata';
+import type { ResourceIdentifier } from '../../../src/ldp/representation/ResourceIdentifier';
+import { GlobalQuotaStrategy } from '../../../src/storage/quota/GlobalQuotaStrategy';
+import type { Size } from '../../../src/storage/size-reporter/Size';
+import type { SizeReporter } from '../../../src/storage/size-reporter/SizeReporter';
+import { guardedStreamFrom, pipeSafely } from '../../../src/util/StreamUtil';
+
+describe('GlobalQuotaStrategy', (): void => {
+  let strategy: GlobalQuotaStrategy;
+  let mockSize: Size;
+  let mockReporter: SizeReporter;
+  let mockBase: string;
+
+  beforeEach((): void => {
+    mockSize = { amount: 2000, unit: 'bytes' };
+    mockBase = '';
+    mockReporter = {
+      getSize: async(identifier: ResourceIdentifier): Promise<Size> => ({
+        unit: mockSize.unit,
+        // This mock will return 1000 as size of the root and 50 for any other resource
+        amount: identifier.path === mockBase ? 1000 : 50,
+      }),
+      getUnit: (): string => mockSize.unit,
+    };
+    strategy = new GlobalQuotaStrategy(mockSize, mockReporter, mockBase);
+  });
+
+  describe('constructor()', (): void => {
+    it('should set the passed parameters as properties.', async(): Promise<void> => {
+      expect(strategy.limit).toEqual(mockSize);
+      expect((strategy as any).reporter).toEqual(mockReporter);
+      expect((strategy as any).base).toBe(mockBase);
+    });
+  });
+
+  describe('getAvailableSpace()', (): void => {
+    it('should return the correct amount of available space left.', async(): Promise<void> => {
+      const result = strategy.getAvailableSpace({ path: 'any/path' });
+      await expect(result).resolves.toEqual(
+        expect.objectContaining({ amount: mockSize.amount - 950 }),
+      );
+    });
+  });
+
+  describe('estimateSize()', (): void => {
+    it('should return a Size object containing the content-length as amount.', async(): Promise<void> => {
+      const metadata = new RepresentationMetadata();
+      metadata.contentLength = 100;
+      await expect(strategy.estimateSize(metadata)).resolves.toEqual(
+        expect.objectContaining({ amount: 100 }),
+      );
+    });
+
+    it(
+      'should return a Size object containing 0 as amount if no content-length is present in the metadata.',
+      async(): Promise<void> => {
+        const metadata = new RepresentationMetadata();
+        await expect(strategy.estimateSize(metadata)).resolves.toEqual(
+          expect.objectContaining({ amount: 0 }),
+        );
+      },
+    );
+
+    it('should return a Size object containing the correct unit.', async(): Promise<void> => {
+      const metadata = new RepresentationMetadata();
+      await expect(strategy.estimateSize(metadata)).resolves.toEqual(
+        expect.objectContaining({ unit: mockSize.unit }),
+      );
+    });
+  });
+
+  describe('trackAvailableSpace()', (): void => {
+    it('should return a passthrough that destroys the stream when quota is exceeded.', async(): Promise<void> => {
+      const fiftyChars = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+      const stream = guardedStreamFrom(fiftyChars);
+      const track = await strategy.trackAvailableSpace({ path: 'any/path' });
+      const piped = pipeSafely(stream, track);
+
+      for (let i = 0; i < 10; i++) {
+        stream.push(fiftyChars);
+      }
+
+      expect(piped.destroyed).toBe(false);
+
+      for (let i = 0; i < 10; i++) {
+        stream.push(fiftyChars);
+      }
+
+      expect(piped.destroyed).toBe(false);
+
+      stream.push(fiftyChars);
+
+      const destroy = new Promise<void>((resolve): void => {
+        piped.on('error', (): void => resolve());
+      });
+      await expect(destroy).resolves.toBeUndefined();
+    });
+  });
+});
