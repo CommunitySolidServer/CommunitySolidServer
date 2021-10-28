aba14a462c9566dbe3389e9fe636b8b19ffb62b8
Joachim Van Herwegen <joachimvh@gmail.com>
2021-10-28 14:03:03 +0200

    feat: Add "no conversion" as possible path in ChainedConverter

--- src/storage/conversion/ChainedConverter.ts
+++ src/storage/conversion/ChainedConverter.ts
@@ -1,5 +1,7 @@
+import { BasicRepresentation } from '../../http/representation/BasicRepresentation';
 import type { Representation } from '../../http/representation/Representation';
-import type { ValuePreference, ValuePreferences } from '../../http/representation/RepresentationPreferences';
+import { RepresentationMetadata } from '../../http/representation/RepresentationMetadata';
+import type { ValuePreferences } from '../../http/representation/RepresentationPreferences';
 import { getLoggerFor } from '../../logging/LogUtil';
 import { BadRequestHttpError } from '../../util/errors/BadRequestHttpError';
 import { NotImplementedHttpError } from '../../util/errors/NotImplementedHttpError';
@@ -14,24 +16,35 @@ type ConverterPreference = {
   outTypes: ValuePreferences;
 };
 
-/**
- * A chain of converters that can go from `inTypes` to `outTypes`.
- * `intermediateTypes` contains the exact types that have the highest weight when going from converter i to i+1.
- */
 type ConversionPath = {
+  /**
+   * List of converters used in the path.
+   */
   converters: TypedRepresentationConverter[];
+  /**
+   * The intermediate conversion types when going from converter i to i+1.
+   * Length is one less than the list of converters.
+   */
   intermediateTypes: string[];
+  /**
+   * The type on which this conversion path starts.
+   */
   inType: string;
+  /**
+   * The types this path can generate.
+   * Weights indicate the quality of transforming to that specific type.
+   */
   outTypes: ValuePreferences;
-};
-
-/**
- * The result of choosing a specific output for a `ConversionPath`.
- */
-type MatchedPath = {
-  path: ConversionPath;
-  outType: string;
+  /**
+   * The weight of the path matched against the output preferences.
+   * Will be 0 if the path does not match any of those preferences.
+   */
   weight: number;
+  /**
+   * The output type for which this path has the highest weight.
+   * Value is irrelevant if weight is 0.
+   */
+  outType: string;
 };
 
 /**
@@ -52,6 +65,9 @@ type MatchedPath = {
  *  - The algorithm could start on both ends of a possible path and work towards the middle.
  *  - When creating a path, store the list of unused converters instead of checking every step.
  *  - Caching: https://github.com/solid/community-server/issues/832
+ *  - Making sure each intermediate type is only used once.
+ *  - The TypedRepresentationConverter interface could potentially be updated
+ *    so paths only differing in intermediate types can be combined.
  */
 export class ChainedConverter extends RepresentationConverter {
   protected readonly logger = getLoggerFor(this);
@@ -76,43 +92,25 @@ export class ChainedConverter extends RepresentationConverter {
   public async handle(input: RepresentationConverterArgs): Promise<Representation> {
     const match = await this.findPath(input);
 
-    // No conversion needed
-    if (!this.isMatchedPath(match)) {
-      return input.representation;
-    }
-
-    const { path, outType } = match;
-    this.logger.debug(`Converting ${path.inType} -> ${[ ...path.intermediateTypes, outType ].join(' -> ')}.`);
+    this.logger.debug(`Converting ${match.inType} -> ${[ ...match.intermediateTypes, match.outType ].join(' -> ')}.`);
 
     const args = { ...input };
-    for (let i = 0; i < path.converters.length - 1; ++i) {
-      const type = path.intermediateTypes[i];
-      args.preferences = { type: { [type]: 1 }};
-      args.representation = await path.converters[i].handle(args);
+    const outTypes = [ ...match.intermediateTypes, match.outType ];
+    for (let i = 0; i < match.converters.length; ++i) {
+      args.preferences = { type: { [outTypes[i]]: 1 }};
+      args.representation = await match.converters[i].handle(args);
     }
-    // For the last converter we set the preferences to the best output type
-    args.preferences = { type: { [outType]: 1 }};
-    return path.converters.slice(-1)[0].handle(args);
-  }
-
-  private isMatchedPath(path: unknown): path is MatchedPath {
-    return typeof (path as MatchedPath).path === 'object';
+    return args.representation;
   }
 
   /**
    * Finds a conversion path that can handle the given input.
    */
-  private async findPath(input: RepresentationConverterArgs): Promise<MatchedPath | ValuePreference> {
+  private async findPath(input: RepresentationConverterArgs): Promise<ConversionPath> {
     const type = input.representation.metadata.contentType!;
     const preferences = cleanPreferences(input.preferences.type);
 
-    const weight = getTypeWeight(type, preferences);
-    if (weight > 0) {
-      this.logger.debug(`No conversion required: ${type} already matches ${preferencesToString(preferences)}`);
-      return { value: type, weight };
-    }
-
-    return this.generatePath(type, preferences);
+    return this.generatePath(type, preferences, input.representation.metadata);
   }
 
   /**
@@ -121,33 +119,33 @@ export class ChainedConverter extends RepresentationConverter {
    *
    * Errors if such a path does not exist.
    */
-  private async generatePath(inType: string, outPreferences: ValuePreferences): Promise<MatchedPath> {
-    // Generate paths from all converters that match the input type
-    let paths = await this.converters.reduce(async(matches: Promise<ConversionPath[]>, converter):
-    Promise<ConversionPath[]> => {
-      const outTypes = await converter.getOutputTypes(inType);
-      if (Object.keys(outTypes).length > 0) {
-        (await matches).push({
-          converters: [ converter ],
-          intermediateTypes: [],
-          inType,
-          outTypes,
-        });
-      }
-      return matches;
-    }, Promise.resolve([]));
+  private async generatePath(inType: string, outPreferences: ValuePreferences, metadata: RepresentationMetadata):
+  Promise<ConversionPath> {
+    //
+    const weight = getTypeWeight(inType, outPreferences);
+    let paths: ConversionPath[] = [{
+      converters: [],
+      intermediateTypes: [],
+      inType,
+      outTypes: { [inType]: 1 },
+      weight,
+      outType: inType,
+    }];
 
     // It's impossible for a path to have a higher weight than this value
     const maxWeight = Math.max(...Object.values(outPreferences));
 
-    let bestPath = this.findBest(outPreferences, paths);
-    paths = this.removeBadPaths(paths, maxWeight, bestPath);
+    // This metadata will be used to simulate canHandle checks
+    const metadataPlaceholder = new RepresentationMetadata(metadata);
+
+    let bestPath = this.findBest(paths);
     // This will always stop at some point since paths can't have the same converter twice
     while (paths.length > 0) {
       // For every path, find all the paths that can be made by adding 1 more converter
-      const promises = paths.map(async(path): Promise<ConversionPath[]> => this.takeStep(path));
+      const promises = paths.map(async(path): Promise<ConversionPath[]> => this.takeStep(path, metadataPlaceholder));
       paths = (await Promise.all(promises)).flat();
-      const newBest = this.findBest(outPreferences, paths);
+      this.updatePathWeights(paths, outPreferences);
+      const newBest = this.findBest(paths);
       if (newBest && (!bestPath || newBest.weight > bestPath.weight)) {
         bestPath = newBest;
       }
@@ -155,12 +153,25 @@ export class ChainedConverter extends RepresentationConverter {
     }
 
     if (!bestPath) {
-      this.logger.warn(`No conversion path could be made from ${inType} to ${Object.keys(outPreferences)}.`);
+      this.logger.warn(`No conversion path could be made from ${inType} to ${preferencesToString(outPreferences)}.`);
       throw new NotImplementedHttpError(
-        `No conversion path could be made from ${inType} to ${Object.keys(outPreferences)}.`,
+        `No conversion path could be made from ${inType} to ${preferencesToString(outPreferences)}.`,
       );
     }
     return bestPath;
+  }
+
+  /**
+   * Checks if a path can match the requested preferences and updates the type and weight if it can.
+   */
+  private updatePathWeights(paths: ConversionPath[], outPreferences: ValuePreferences): void {
+    for (const path of paths) {
+      const outMatch = getBestPreference(path.outTypes, outPreferences);
+      if (outMatch && outMatch.weight > 0) {
+        path.weight = outMatch.weight;
+        path.outType = outMatch.value;
+      }
+    }
   }
 
   /**
@@ -168,13 +179,11 @@ export class ChainedConverter extends RepresentationConverter {
    * If there are multiple matches the one with the highest result weight gets chosen.
    * Will return undefined if there are no matches.
    */
-  private findBest(preferences: ValuePreferences, paths: ConversionPath[]): MatchedPath | undefined {
+  private findBest(paths: ConversionPath[]): ConversionPath | undefined {
     // Need to use null instead of undefined so `reduce` doesn't take the first element of the array as `best`
-    return paths.reduce((best: MatchedPath | null, path): MatchedPath | null => {
-      const outMatch = getBestPreference(path.outTypes, preferences);
-      if (outMatch && !(best && best.weight >= outMatch.weight)) {
-        // Create new MatchedPath, using the output match above
-        return { path, outType: outMatch.value, weight: outMatch.weight };
+    return paths.reduce((best: ConversionPath | null, path): ConversionPath | null => {
+      if (path.weight > 0 && !(best && best.weight >= path.weight)) {
+        return path;
       }
       return best;
     }, null) ?? undefined;
@@ -188,7 +197,7 @@ export class ChainedConverter extends RepresentationConverter {
    * @param maxWeight - The maximum weight in the output preferences.
    * @param bestMatch - The current best path.
    */
-  private removeBadPaths(paths: ConversionPath[], maxWeight: number, bestMatch?: MatchedPath): ConversionPath[] {
+  private removeBadPaths(paths: ConversionPath[], maxWeight: number, bestMatch?: ConversionPath): ConversionPath[] {
     // All paths are still good if there is no best match yet
     if (!bestMatch) {
       return paths;
@@ -209,16 +218,19 @@ export class ChainedConverter extends RepresentationConverter {
    * Finds all converters that could take the output of the given path as input.
    * For each of these converters a new path gets created which is the input path appended by the converter.
    */
-  private async takeStep(path: ConversionPath): Promise<ConversionPath[]> {
+  private async takeStep(path: ConversionPath, metadata: RepresentationMetadata): Promise<ConversionPath[]> {
     const unusedConverters = this.converters.filter((converter): boolean => !path.converters.includes(converter));
-    const nextConverters = await this.supportedConverters(path.outTypes, unusedConverters);
+    const nextConverters = await this.supportedConverters(path.outTypes, metadata, unusedConverters);
 
     // Create a new path for every converter that can be appended
     return Promise.all(nextConverters.map(async(pref): Promise<ConversionPath> => ({
       converters: [ ...path.converters, pref.converter ],
-      intermediateTypes: [ ...path.intermediateTypes, pref.inType ],
+      intermediateTypes: path.converters.length > 0 ? [ ...path.intermediateTypes, pref.inType ] : [],
       inType: path.inType,
       outTypes: pref.outTypes,
+      // These will be updated later
+      weight: 0,
+      outType: 'invalid',
     })));
   }
 
@@ -232,18 +244,43 @@ export class ChainedConverter extends RepresentationConverter {
 
   /**
    * Finds all converters in the given list that support taking any of the given types as input.
+   * Filters out converters that would produce an already seen type.
    */
-  private async supportedConverters(types: ValuePreferences, converters: TypedRepresentationConverter[]):
-  Promise<ConverterPreference[]> {
+  private async supportedConverters(types: ValuePreferences, metadata: RepresentationMetadata,
+    converters: TypedRepresentationConverter[]): Promise<ConverterPreference[]> {
     const typeEntries = Object.entries(types);
     const results: ConverterPreference[] = [];
     for (const converter of converters) {
       for (const [ inType, weight ] of typeEntries) {
-        let outTypes = await converter.getOutputTypes(inType);
-        outTypes = this.modifyTypeWeights(weight, outTypes);
-        results.push({ converter, inType, outTypes });
+        // This metadata object is only used internally so changing the content-type is fine
+        metadata.contentType = inType;
+        const preference = await this.findConverterPreference(inType, weight, metadata, converter);
+        if (preference) {
+          results.push(preference);
+        }
       }
     }
     return results;
+  }
+
+  /**
+   * Returns a ConverterPreference if the given converter supports the given type.
+   * All types that have already been used will be removed from the output types.
+   */
+  private async findConverterPreference(inType: string, weight: number, metadata: RepresentationMetadata,
+    converter: TypedRepresentationConverter): Promise<ConverterPreference | undefined> {
+    const representation = new BasicRepresentation([], metadata);
+    try {
+      const identifier = { path: representation.metadata.identifier.value };
+      // Internal types get ignored when trying to match everything, so they need to be specified to also match.
+      await converter.canHandle({ representation, identifier, preferences: { type: { '*/*': 1, 'internal/*': 1 }}});
+    } catch {
+      // Skip converters that fail the canHandle test
+      return;
+    }
+
+    let outTypes = await converter.getOutputTypes(inType);
+    outTypes = this.modifyTypeWeights(weight, outTypes);
+    return { converter, inType, outTypes };
   }
 }
