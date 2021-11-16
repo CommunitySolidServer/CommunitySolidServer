fe4dd4b2e5972ffa1b53dea65acb3c5b2e2db67f
Arthur Joppart <arthur@digita.ai>
2021-11-16 15:25:39 +0100

    feat: added PasstroughtDataAccessor + tests

--- test/unit/storage/accessors/PassthroughDataAccessor.test.ts
+++ test/unit/storage/accessors/PassthroughDataAccessor.test.ts
@@ -33,17 +33,59 @@ describe('ValidatingDataAccessor', (): void => {
   });
 
   describe('writeDocument()', (): void => {
-    it('should call the validator\'s handle() function.', async(): Promise<void> => {
-      const spy = jest.spyOn(validator, 'handle');
-      await validatingAccessor.writeDocument(mockIdentifier, mockData, mockMetadata);
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(spy).toHaveBeenCalledWith({ representation: mockRepresentation, identifier: mockIdentifier });
-    });
     it('should call the accessors writeDocument() function.', async(): Promise<void> => {
       const spy = jest.spyOn(childAccessor, 'writeDocument');
       await validatingAccessor.writeDocument(mockIdentifier, mockData, mockMetadata);
       expect(spy).toHaveBeenCalledTimes(1);
       expect(spy).toHaveBeenCalledWith(mockIdentifier, mockData, mockMetadata);
+    });
+  });
+  describe('canHandle()', (): void => {
+    it('should call the accessors canHandle() function.', async(): Promise<void> => {
+      const spy = jest.spyOn(childAccessor, 'canHandle');
+      await validatingAccessor.canHandle(mockRepresentation);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(mockRepresentation);
+    });
+  });
+  describe('getData()', (): void => {
+    it('should call the accessors getData() function.', async(): Promise<void> => {
+      const spy = jest.spyOn(childAccessor, 'getData');
+      await validatingAccessor.getData(mockIdentifier);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(mockIdentifier);
+    });
+  });
+  describe('getMetadata()', (): void => {
+    it('should call the accessors getMetadata() function.', async(): Promise<void> => {
+      const spy = jest.spyOn(childAccessor, 'getMetadata');
+      await validatingAccessor.getMetadata(mockIdentifier);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(mockIdentifier);
+    });
+  });
+  describe('getChildren()', (): void => {
+    it('should call the accessors getChildren() function.', async(): Promise<void> => {
+      const spy = jest.spyOn(childAccessor, 'getChildren');
+      validatingAccessor.getChildren(mockIdentifier);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(mockIdentifier);
+    });
+  });
+  describe('deleteResource()', (): void => {
+    it('should call the accessors deleteResource() function.', async(): Promise<void> => {
+      const spy = jest.spyOn(childAccessor, 'deleteResource');
+      await validatingAccessor.deleteResource(mockIdentifier);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(mockIdentifier);
+    });
+  });
+  describe('writeContainer()', (): void => {
+    it('should call the accessors writeContainer() function.', async(): Promise<void> => {
+      const spy = jest.spyOn(childAccessor, 'writeContainer');
+      await validatingAccessor.writeContainer(mockIdentifier, mockMetadata);
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith(mockIdentifier, mockMetadata);
     });
   });
 });
