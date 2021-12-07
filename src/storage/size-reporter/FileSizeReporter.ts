b7f4a4348b2410f051419b2dcda3190f20d54aa2
Arthur Joppart <arthur@digita.ai>
2021-12-07 10:52:53 +0100

    test: changed global quota parameters

--- src/storage/size-reporter/FileSizeReporter.ts
+++ src/storage/size-reporter/FileSizeReporter.ts
@@ -58,8 +58,6 @@ export class FileSizeReporter implements SizeReporter<string> {
       return 0;
     }
 
-    console.log('statSize', { fileLocation, size: stat.size });
-
     // If the file's location points to a file, simply return the file's size
     if (stat.isFile()) {
       return stat.size;
