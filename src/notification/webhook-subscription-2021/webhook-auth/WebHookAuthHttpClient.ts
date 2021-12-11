6c52bc450b15bf2492b3004287b5820c2c43f4a7
jaxoncreed <jaxoncreed@gmail.com>
2021-12-10 19:49:40 -0800

    Removed trailing slash

--- src/notification/webhook-subscription-2021/webhook-auth/WebHookAuthHttpClient.ts
+++ src/notification/webhook-subscription-2021/webhook-auth/WebHookAuthHttpClient.ts
@@ -8,6 +8,7 @@ import { SignJWT } from 'jose/jwt/sign';
 import type { HttpClient } from '../../../http/client/HttpClient';
 import type { JwksKeyGenerator } from '../../../identity/configuration/JwksKeyGenerator';
 import { InternalServerError } from '../../../util/errors/InternalServerError';
+import { trimTrailingSlashes } from '../../../util/PathUtil';
 import { POD_JWKS_KEY } from './PodJwksHttpHandler';
 
 export interface WebHookAuthHttpClientArgs {
@@ -30,44 +31,48 @@ export class WebHookAuthHttpClient implements HttpClient {
     data: any,
     callback?: ((res: IncomingMessage) => void) | undefined,
   ): void {
-    this.jwksKeyGenerator.getPrivateJwks(POD_JWKS_KEY).then((jwks: { keys: any[] }): void => {
-      const jwk = jwks.keys[0];
-      if (!jwk) {
-        throw new InternalServerError('No jwk available.');
-      }
-      parseJwk(jwk, 'RS256').then((jwkKeyLike): void => {
-        const jwtRaw = {
-          htu: url,
-          htm: 'POST',
-        };
-        new SignJWT(jwtRaw)
-          .setProtectedHeader({ alg: 'RS256' })
-          .setIssuedAt()
-          .setIssuer(this.baseUrl)
-          .setExpirationTime('20m')
-          .sign(jwkKeyLike)
-          .then((signedJwt: string): void => {
-            const augmentedOptions: RequestOptions = {
-              ...options,
-              headers: {
-                ...options.headers,
-                authorization: signedJwt,
-              },
+    this.jwksKeyGenerator
+      .getPrivateJwks(POD_JWKS_KEY)
+      .then((jwks: { keys: any[] }): void => {
+        const jwk = jwks.keys[0];
+        if (!jwk) {
+          throw new InternalServerError('No jwk available.');
+        }
+        parseJwk(jwk, 'RS256')
+          .then((jwkKeyLike): void => {
+            const jwtRaw = {
+              htu: url,
+              htm: 'POST',
             };
+            new SignJWT(jwtRaw)
+              .setProtectedHeader({ alg: 'RS256' })
+              .setIssuedAt()
+              .setIssuer(trimTrailingSlashes(this.baseUrl))
+              .setExpirationTime('20m')
+              .sign(jwkKeyLike)
+              .then((signedJwt: string): void => {
+                const augmentedOptions: RequestOptions = {
+                  ...options,
+                  headers: {
+                    ...options.headers,
+                    authorization: signedJwt,
+                  },
+                };
 
-            const req = request(url, augmentedOptions, callback);
-            req.write(data);
-            req.end();
+                const req = request(url, augmentedOptions, callback);
+                req.write(data);
+                req.end();
+              })
+              .catch((err: unknown): void => {
+                throw err;
+              });
           })
           .catch((err: unknown): void => {
             throw err;
           });
       })
-        .catch((err: unknown): void => {
-          throw err;
-        });
-    }).catch((err: unknown): void => {
-      throw err;
-    });
+      .catch((err: unknown): void => {
+        throw err;
+      });
   }
 }
