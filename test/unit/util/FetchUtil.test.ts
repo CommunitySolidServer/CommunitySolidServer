f1afc60001265e4a20245b073b1b86222b22a603
Thomas Dupont <thomas.dupont@ugent.be>
2022-02-15 11:14:34 +0100

    test(FetchUtil): return Response object instead of mocking fetch

--- test/unit/util/FetchUtil.test.ts
+++ test/unit/util/FetchUtil.test.ts
@@ -1,20 +1,28 @@
 import { Readable } from 'stream';
 import type { Quad } from '@rdfjs/types';
 import arrayifyStream from 'arrayify-stream';
-import { fetch } from 'cross-fetch';
+import type { Response } from 'cross-fetch';
 import { DataFactory } from 'n3';
 import rdfDereferencer from 'rdf-dereference';
 import { RdfToQuadConverter } from '../../../src/storage/conversion/RdfToQuadConverter';
 import { fetchDataset, responseToDataset } from '../../../src/util/FetchUtil';
 const { namedNode, quad } = DataFactory;
 
-jest.mock('cross-fetch');
 jest.mock('rdf-dereference', (): any => ({
   dereference: jest.fn<string, any>(),
 }));
 
 describe('FetchUtil', (): void => {
   const url = 'http://test.com/foo';
+
+  function mockResponse(body: string, contentType: string | null, status = 200): Response {
+    return ({
+      text: (): any => body,
+      url,
+      status,
+      headers: { get: (): any => contentType },
+    }) as any;
+  }
 
   describe('#fetchDataset', (): void => {
     const rdfDereferenceMock: jest.Mocked<typeof rdfDereferencer> = rdfDereferencer as any;
@@ -49,21 +57,10 @@ describe('FetchUtil', (): void => {
   });
 
   describe('#responseToDataset', (): void => {
-    const fetchMock: jest.Mock = fetch as any;
     const converter = new RdfToQuadConverter();
 
-    function mockFetch(body: string, status = 200): void {
-      fetchMock.mockImplementation((input: string): any => ({
-        text: (): any => body,
-        url: input,
-        status,
-        headers: { get: (): any => 'text/turtle' },
-      }));
-    }
-
     it('accepts Response objects as input.', async(): Promise<void> => {
-      mockFetch('<http://test.com/s> <http://test.com/p> <http://test.com/o>.');
-      const response = await fetch(url);
+      const response = mockResponse('<http://test.com/s> <http://test.com/p> <http://test.com/o>.', 'text/turtle');
       const body = await response.text();
       const representation = await responseToDataset(response, converter, body);
       await expect(arrayifyStream(representation.data)).resolves.toEqual([
@@ -72,15 +69,13 @@ describe('FetchUtil', (): void => {
     });
 
     it('errors if the status code is not 200.', async(): Promise<void> => {
-      mockFetch('Incorrect status!', 400);
-      await expect(responseToDataset(await fetch(url), converter)).rejects.toThrow(`Unable to access data at ${url}`);
-      expect(fetchMock).toHaveBeenCalledWith(url);
+      const response = mockResponse('Incorrect status!', null, 400);
+      await expect(responseToDataset(response, converter)).rejects.toThrow(`Unable to access data at ${url}`);
     });
 
     it('errors if there is no content-type.', async(): Promise<void> => {
-      fetchMock.mockResolvedValueOnce({ url, text: (): any => '', status: 200, headers: { get: jest.fn() }});
-      await expect(responseToDataset(await fetch(url), converter)).rejects.toThrow(`Unable to access data at ${url}`);
-      expect(fetchMock).toHaveBeenCalledWith(url);
+      const response = mockResponse('No content-type!', null);
+      await expect(responseToDataset(response, converter)).rejects.toThrow(`Unable to access data at ${url}`);
     });
   });
 });
