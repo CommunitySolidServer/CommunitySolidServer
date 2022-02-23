eceb71088ac2790900e6143ab23519f0d42925e7
Ruben Verborgh <ruben@verborgh.org>
2022-02-23 10:40:14 +0100

    docs: Explain why IDP redirects are transformed into JSON responses.

--- src/identity/interaction/LocationInteractionHandler.ts
+++ src/identity/interaction/LocationInteractionHandler.ts
@@ -6,13 +6,21 @@ import type { InteractionHandlerInput } from './InteractionHandler';
 import { InteractionHandler } from './InteractionHandler';
 
 /**
- * Catches redirect errors from the source and returns a JSON body containing a `location` field instead.
- * This allows the API to be used more easily from the browser.
+ * Transforms an HTTP redirect into a hypermedia document with a redirection link,
+ * such that scripts running in a browser can redirect the user to the next page.
  *
- * The issue is that if the API actually did a redirect,
- * this would make it unusable when using it on HTML pages that need to render errors in case the fetch fails,
- * but want to redirect the page in case it succeeds.
- * See full overview at https://github.com/solid/community-server/pull/1088.
+ * This handler addresses the situation where:
+ * - the user visits a first page
+ * - this first page contains a script that performs interactions with a JSON API
+ * - as a result of a certain interaction, the user needs to be redirected to a second page
+ *
+ * Regular HTTP redirects are performed via responses with 3xx status codes.
+ * However, since the consumer of the API in this case is a browser script,
+ * a 3xx response would only reach that script and not move the page for the user.
+ *
+ * Therefore, this handler changes a 3xx response into a 200 response
+ * with an explicit link to the next page,
+ * enabling the script to move the user to the next page.
  */
 export class LocationInteractionHandler extends InteractionHandler {
   private readonly source: InteractionHandler;
