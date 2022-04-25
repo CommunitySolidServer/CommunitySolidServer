25ceb35861a763d43aec6a26aa0242e4869fd845
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-04-25 11:21:18 +0200

    chore: changes after review

--- test/deploy/validate-configs.sh
+++ test/deploy/validate-configs.sh
@@ -2,13 +2,15 @@
 # Script to validate the packaged module and configs
 
 # Ensure our workdir is that of the project root
-cd "${0%/*}/../.." || exit 
+cd "${0%/*}/../.." || echo "Error setting workdir to project directory." || exit 1
 
+# No arguments: all default configs will be tested
+# One or more arguments: 
+#   Example: validate-configs.sh default file.json config/sparql-endpoint.json
 TEST_NAME="Deployment testing"
 declare -a CONFIG_ARRAY
 if [[ $# -gt 0 ]]; then
     for CONFIG in "$@"; do
-      CONFIG=$(ls {"$CONFIG",config/"$CONFIG",config/"$CONFIG".json,"$CONFIG".json} 2> /dev/null)
       if [ ! -f "$CONFIG" ]; then
         echo "Config file $CONFIG does not exist, check the path."
         exit 1
@@ -22,7 +24,7 @@ else
 fi
 printf " - %s\n" "${CONFIG_ARRAY[@]}"
 
-mkdir -p .tmp
+mkdir -p test/tmp/data 
 echo "$TEST_NAME - Building and installing package"
 npm pack --loglevel warn
 npm install -g solid-community-server-*.tgz --loglevel warn
@@ -39,47 +41,47 @@ run_server_with_config () {
   CONFIG_PATH=$1
   CONFIG_NAME=$2
 
-  mkdir -p .data
+  mkdir -p test/tmp/data
 
-  CSS_ARGS="-p 8888 -l warn -f .data/ -s http://localhost:4000/sparql"
+  CSS_ARGS="-p 8888 -l warn -f test/tmp/data -s http://localhost:4000/sparql"
   CSS_BASE_URL="http://localhost:8888"
 
   # HTTPS config specifics: self-signed key/cert + CSS base URL override
   if [[ $CONFIG_NAME =~ "https" ]]; then
-    openssl req -x509 -nodes -days 1 -newkey rsa:2048 -keyout .tmp/server.key -out .tmp/server.cert -subj '/CN=localhost' &>/dev/null
+    openssl req -x509 -nodes -days 1 -newkey rsa:2048 -keyout test/tmp/server.key -out test/tmp/server.cert -subj '/CN=localhost' &>/dev/null
     CSS_BASE_URL="https://localhost:8888"
     if [[ $CONFIG_NAME =~ "https-file-cli" ]]; then
-      CSS_ARGS="$CSS_ARGS --httpsKey .tmp/server.key --httpsCert .tmp/server.cert"
+      CSS_ARGS="$CSS_ARGS --httpsKey test/tmp/server.key --httpsCert test/tmp/server.cert"
     elif [[ $CONFIG_NAME =~ "example-https" ]]; then
-      cp config/example-https-file.json .tmp/example-https-file.json
-      sed -i -E "s/(\W+\"options_key\".*\").+(\".*)/\1.tmp\/server.key\2/" config/example-https-file.json
-      sed -i -E "s/(\W+\"options_cert\".*\").+(\".*)/\1.tmp\/server.cert\2/" config/example-https-file.json
+      CONFIG_PATH=test/tmp/example-https-file.json
+      cp config/example-https-file.json $CONFIG_PATH
+      sed -i -E "s/(\W+\"options_key\".*\").+(\".*)/\1test\/tmp\/server.key\2/" $CONFIG_PATH
+      sed -i -E "s/(\W+\"options_cert\".*\").+(\".*)/\1test\/tmp\/server.cert\2/" $CONFIG_PATH
     fi
   fi
 
   echo -e "----------------------------------"
   echo "$TEST_NAME($CONFIG_NAME) - Starting the server"
-  community-solid-server "$CSS_ARGS" -b $CSS_BASE_URL -c "$CONFIG_PATH"  &>.tmp/"$CONFIG_NAME" &
+  community-solid-server "$CSS_ARGS" -b $CSS_BASE_URL -c "$CONFIG_PATH"  &>test/tmp/"$CONFIG_NAME" &
   PID=$!
 
   FAILURE=1
   if [ -z $PID ]; then
     echo "$TEST_NAME($CONFIG_NAME) - FAILURE: Server did not start"
-    cat .tmp/"$CONFIG_NAME"
+    cat test/tmp/"$CONFIG_NAME"
   else
     echo "$TEST_NAME($CONFIG_NAME) - Attempting HTTP access to the server"
-    if curl -sfkI -X GET --retry 15 --retry-connrefused --retry-delay 1 $CSS_BASE_URL > .tmp/"$CONFIG_NAME"-curl; then
+    if curl -sfkI -X GET --retry 15 --retry-connrefused --retry-delay 1 $CSS_BASE_URL > test/tmp/"$CONFIG_NAME"-curl; then
       echo "$TEST_NAME($CONFIG_NAME) - SUCCESS: server reached"
       FAILURE=0
-    fi
-    if [ $FAILURE -eq 1 ]; then
+    else
       echo "$TEST_NAME($CONFIG_NAME) - FAILURE: Could not reach server"
     fi
     kill -9 $PID &> /dev/null
     timeout 30s tail --pid=$PID -f /dev/null
   fi
 
-  rm -rf .data/*
+  rm -rf test/tmp/data/*
   return $FAILURE
 }
 
@@ -95,9 +97,9 @@ for CONFIG_PATH in "${CONFIG_ARRAY[@]}"; do
     SUCCESSES="${SUCCESSES}[SUCCESS]\t$CONFIG_NAME\t($CONFIG_PATH)\n"
   else
     echo "$TEST_NAME($CONFIG_NAME) - CSS logs: ";
-    cat .tmp/"$CONFIG_NAME"
+    cat test/tmp/"$CONFIG_NAME"
     echo "$TEST_NAME($CONFIG_NAME) - curl logs: ";
-    cat .tmp/"$CONFIG_NAME"-curl.log
+    cat test/tmp/"$CONFIG_NAME"-curl
     VALIDATION_FAILURE=1
     FAILURES="${FAILURES}[FAILURE]\t$CONFIG_NAME\t($CONFIG_PATH)\n"
   fi
@@ -106,8 +108,7 @@ done;
 
 echo "$TEST_NAME - Cleanup"
 npm uninstall -g @solid/community-server
-mv .tmp/example-https-file.json config/example-https-file.json 2>/dev/null || true
-rm -rf .tmp .data
+rm -rf test/tmp/*
 
 
 echo -e "\n\n----------------------------------------"
