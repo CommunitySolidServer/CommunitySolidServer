76ec9631eff803a40e309dd97d26b9efdf26ca46
Joachim Van Herwegen <joachimvh@gmail.com>
2022-05-10 09:59:46 +0200

    chore: Update commit linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'never',
+      ['start-case', 'kebab-case', 'snake-case'],
+    ],
+  }
+};
