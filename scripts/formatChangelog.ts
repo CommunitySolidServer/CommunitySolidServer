c564b435ad811a769174d13cdbd9025b1b215d32
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-05-13 13:57:39 +0200

    docs: add some documentation comments to script

--- scripts/formatChangelog.ts
+++ scripts/formatChangelog.ts
@@ -2,6 +2,20 @@
 /* eslint-disable no-console */
 import { readFile, writeFile } from 'fs-extra';
 
+/**
+ * Script called after the changelog changes of standard-version
+ * This script can be extended to add further custom formatting
+ * to the changelog.
+ * Current automatic changes:
+ *  - Change all version titles to H2 ( ### [vX.Y.Z] => ## [vX.Y.Z])
+ */
+
+/**
+ * @param from  - Regular expression to search for
+ * @param to    - String to replace to
+ * @param filePath - File to search/replace
+ * @returns Promise
+ */
 async function replaceInFile(from: RegExp, to: string, filePath: string): Promise<void> {
   const data = await readFile(filePath, 'utf8');
   const result = data.replace(from, to);
