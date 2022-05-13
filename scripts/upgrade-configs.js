f7d09bec1cef70ec5513111ab672c9064067dd3e
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-05-13 14:38:13 +0200

    chore: upgrade-config code improvements

--- scripts/upgrade-configs.js
+++ scripts/upgrade-configs.js
@@ -5,37 +5,29 @@ const { fdir } = require("fdir");
 const major = process.env.npm_package_version.split('.')[0]
 
 async function replaceVersion(filePath){
-    return fs.readFile(filePath, 'utf8', function(err,data) {
-        if (err) {
-            return process.stderr.write(`${err}\n`)
-        }
-        var result = data.replace(/(@solid\/community-server\/)\^\d+\.\d+\.\d+/gm, `$1^${major}.0.0`);
-
-        fs.writeFile(filePath, result, 'utf8', function (err) {
-            if (err) return process.stderr.write(`${err}\n`);
-        });
-    })
+    const data = await fs.readFile(filePath, 'utf8');
+    const result = data.replace(/(@solid\/community-server\/)\^\d+\.\d+\.\d+/gm, `$1^${major}.0.0`);
+    return fs.writeFile(filePath, result, 'utf8');
 }
 
 async function upgradeConfig(){
-    process.stdout.write(`Changing @solid/community-server references to ${major}.0.0\n`)
+    console.log(`Changing @solid/community-server references to ${major}.0.0\n`)
     
     const configs = await new fdir().withBasePath()
         .filter((path, isDirectory) => path.endsWith(".json"))
         .crawl('config')
         .withPromise();
-    
+
     for(let index = 0; index < configs.length; index++){
         await replaceVersion(configs[index]);
     }
     await replaceVersion('package-lock.json');
 
-    return await simpleGit().commit(`chore: Update configs to v${major}.0.0`, configs, {'--no-verify': undefined});
+    return simpleGit().commit(`chore: Update configs to v${major}.0.0`, configs, {'--no-verify': undefined});
 }
 
 upgradeConfig()
-    .then(() => process.stdout.write(`Configs upgraded and committed\n`))
+    .then(() => console.log(`Configs upgraded and committed\n`))
     .catch(error => {
-    process.stderr.write(`${error.stack}\n`);
-    process.exit(1);
+    console.error(`${error.stack}`);
     });
