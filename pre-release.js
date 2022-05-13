d59da38226d562e0adcee4c483ed07151548f795
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-05-13 14:38:12 +0200

    style: formatting

--- pre-release.js
+++ pre-release.js
@@ -1,6 +1,5 @@
 const shell = require('shelljs');
 const simpleGit = require('simple-git');
-const git = simpleGit();
 
 const major = process.env.npm_package_version.split('.')[0]
 console.log(`Changing @solid/community-server references to ${major}.0.0`)
@@ -11,5 +10,6 @@ configs.forEach((filePath,index) => {
     shell.sed('-i', /(@solid\/community-server\/)\^\d+\.\d+\.\d+/, `$1^${major}.0.0`, filePath)
 });
 
+const git = simpleGit();
 git.add(configs);
-// git.commit(`chore: Update configs to v${version}`);
+git.commit(`chore: Update configs to v${version}`);
