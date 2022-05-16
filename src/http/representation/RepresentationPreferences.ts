afed963a239c27033d94d4b779ad5848f446e16a
Thomas Dupont <thomas.dupont@ugent.be>
2022-05-16 10:58:56 +0200

    fix: revert ef6f01a

--- src/http/representation/RepresentationPreferences.ts
+++ src/http/representation/RepresentationPreferences.ts
@@ -6,8 +6,12 @@
   * "The weight is normalized to a real number in the range 0 through 1,
   * where 0.001 is the least preferred and 1 is the most preferred; a
   * value of 0 means "not acceptable"."
+  *
+  * Because of an open issue in Components.js we cannot use `Record<string, number>` right now.
+  * https://github.com/LinkedSoftwareDependencies/Components-Generator.js/issues/103
   */
-export type ValuePreferences = Record<string, number>;
+// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
+export type ValuePreferences = {[key: string ]: number };
 
 /**
  * A single entry of a {@link ValuePreferences} object.
