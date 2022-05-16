458d4c806b53667a18c4429950e6f69f943efb35
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-05-16 13:21:49 +0200

    fix(oidc): rewrite requests with rootpath

--- src/identity/OidcHttpHandler.ts
+++ src/identity/OidcHttpHandler.ts
@@ -18,6 +18,13 @@ export class OidcHttpHandler extends HttpHandler {
 
   public async handle({ request, response }: HttpHandlerInput): Promise<void> {
     const provider = await this.providerFactory.getProvider();
+
+    // Rewrite requests to allow hosting on root paths
+    const path = new URL(provider.issuer).pathname;
+    if (path.length > 1 && request.url!.startsWith(`${path}.well-known/openid-configuration`)) {
+      request.url = '/.well-known/openid-configuration';
+    }
+
     this.logger.debug(`Sending request to oidc-provider: ${request.url}`);
     // Even though the typings do not indicate this, this is a Promise that needs to be awaited.
     // Otherwise, the `BaseHttpServerFactory` will write a 404 before the OIDC library could handle the response.
--- test/unit/identity/OidcHttpHandler.test.ts
+++ test/unit/identity/OidcHttpHandler.test.ts
@@ -5,7 +5,9 @@ import type { HttpRequest } from '../../../src/server/HttpRequest';
 import type { HttpResponse } from '../../../src/server/HttpResponse';
 
 describe('An OidcHttpHandler', (): void => {
-  const request: HttpRequest = {} as any;
+  const request: HttpRequest = {
+    url: '/.well-known/openid-configuration',
+  } as any;
   const response: HttpResponse = {} as any;
   let provider: jest.Mocked<Provider>;
   let providerFactory: jest.Mocked<ProviderFactory>;
@@ -14,17 +16,28 @@ describe('An OidcHttpHandler', (): void => {
   beforeEach(async(): Promise<void> => {
     provider = {
       callback: jest.fn().mockReturnValue(jest.fn()),
+      issuer: 'http://localhost:3000/',
     } as any;
 
     providerFactory = {
       getProvider: jest.fn().mockResolvedValue(provider),
-    };
+    } as any;
 
     handler = new OidcHttpHandler(providerFactory);
   });
 
   it('sends all requests to the OIDC library.', async(): Promise<void> => {
     await expect(handler.handle({ request, response })).resolves.toBeUndefined();
+    expect(provider.callback).toHaveBeenCalledTimes(1);
+    expect(provider.callback.mock.results[0].value).toHaveBeenCalledTimes(1);
+    expect(provider.callback.mock.results[0].value).toHaveBeenLastCalledWith(request, response);
+  });
+
+  it('rewrites the request when using base URL with root path.', async(): Promise<void> => {
+    Object.assign(provider, { issuer: 'http://localhost:3000/path/' });
+    request.url = '/path/.well-known/openid-configuration';
+    await expect(handler.handle({ request, response })).resolves.toBeUndefined();
+    expect(request.url).toBe('/.well-known/openid-configuration');
     expect(provider.callback).toHaveBeenCalledTimes(1);
     expect(provider.callback.mock.results[0].value).toHaveBeenCalledTimes(1);
     expect(provider.callback.mock.results[0].value).toHaveBeenLastCalledWith(request, response);
