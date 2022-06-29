f636640737a8ca794ae6eed9d5c37ecf3d09af68
woutslabbinck <wout.slabbinck@ugent.be>
2022-06-29 13:25:21 +0200

    test: add integration tests for shape validation support

--- test/integration/LdpHandlerWithoutAuth.test.ts
+++ test/integration/LdpHandlerWithoutAuth.test.ts
@@ -577,4 +577,96 @@ describe.each(stores)('An LDP handler allowing all requests %s', (name, { storeC
     expect(response.status).toBe(200);
     await expectQuads(response, [ quad(namedNode(baseUrl), namedNode(RDF.type), namedNode(PIM.Storage)) ]);
   });
+
+  const shape = `<http://example.org/exampleshape> a <http://www.w3.org/ns/shacl#NodeShape>.
+<http://example.org/exampleshape> <http://www.w3.org/ns/shacl#targetClass> <http://example.org/c>.
+<http://example.org/exampleshape> <http://www.w3.org/ns/shacl#property> <http://example.org/property>.
+<http://example.org/property> <http://www.w3.org/ns/shacl#path> <http://xmlns.com/foaf/0.1/name>.
+<http://example.org/property> <http://www.w3.org/ns/shacl#minCount> 1.
+<http://example.org/property> <http://www.w3.org/ns/shacl#maxCount> 1.
+<http://example.org/property> <http://www.w3.org/ns/shacl#datatype> <http://www.w3.org/2001/XMLSchema#string>.`;
+  const conformingResource = `<a> a <http://example.org/c>.
+<a> <http://xmlns.com/foaf/0.1/name> "test".`;
+  it('can validate RDF resources using SHACL shapes.', async(): Promise<void> => {
+    const shapeURL = `${baseUrl}shape`;
+    const constrainedContainerURL = `${baseUrl}constrained/`;
+
+    // PUT shape
+    await putResource(shapeURL, { contentType: 'text/turtle', body: shape });
+
+    // PUT container for constrained resources
+    await putResource(constrainedContainerURL, { contentType: 'text/turtle' });
+
+    // PATCH: Add shape constraint to container
+    await patchResource(
+      constrainedContainerURL + metaSuffix,
+      `INSERT DATA { <${constrainedContainerURL}> <http://www.w3.org/ns/ldp#constrainedBy> <${shapeURL}>}`,
+      'sparql',
+      true,
+    );
+
+    const constrainedContainerResponse = await getResource(constrainedContainerURL);
+    expect(constrainedContainerResponse.headers.get('link'))
+      .toContain(`<${shapeURL}>; rel="http://www.w3.org/ns/ldp#constrainedBy"`);
+
+    // POST: Add resource to constrained container which is valid
+    const postResponse = await postResource(
+      constrainedContainerURL, { contentType: 'text/turtle', body: conformingResource },
+    );
+
+    // POST: Add resource to constrained container that is not valid
+    const response1 = await fetch(constrainedContainerURL, {
+      method: 'POST',
+      headers: { 'content-type': 'text/turtle' },
+      body: '<a> <b> <c>.',
+    });
+    expect(response1.status).toBe(400);
+
+    // PUT: Add container resource (invalid)
+    const response2 = await fetch(`${constrainedContainerURL}container/`, {
+      method: 'PUT',
+      headers: { 'content-type': 'text/turtle' },
+    });
+    expect(response2.status).toBe(400);
+
+    // DELETE
+    expect(await deleteResource(postResponse.headers.get('location')!)).toBeUndefined();
+    expect(await deleteResource(constrainedContainerURL)).toBeUndefined();
+    expect(await deleteResource(shapeURL)).toBeUndefined();
+  });
+
+  it('can not validate non-RDF resources using SHACL shapes.', async(): Promise<void> => {
+    const shapeURL = `${baseUrl}shape`;
+    const constrainedContainerURL = `${baseUrl}constrained/`;
+
+    // PUT shape
+    await putResource(shapeURL, { contentType: 'text/turtle', body: shape });
+
+    // PUT container for constrained resources
+    await putResource(constrainedContainerURL, { contentType: 'text/turtle' });
+
+    // PATCH: Add shape constraint to container
+    await patchResource(
+      constrainedContainerURL + metaSuffix,
+      `INSERT DATA { <${constrainedContainerURL}> <http://www.w3.org/ns/ldp#constrainedBy> <${shapeURL}>}`,
+      'sparql',
+      true,
+    );
+
+    const constrainedContainerResponse = await getResource(constrainedContainerURL);
+    expect(constrainedContainerResponse.headers.get('link'))
+      .toContain(`<${shapeURL}>; rel="http://www.w3.org/ns/ldp#constrainedBy"`);
+
+    // POST non-RDF resource
+    const response1 = await fetch(constrainedContainerURL, {
+      method: 'POST',
+      headers: { 'content-type': 'text/plain' },
+      body: 'Hello world!',
+    });
+    expect(response1.status).toBe(400);
+
+    // DELETE
+    expect(await deleteResource(constrainedContainerURL)).toBeUndefined();
+    expect(await deleteResource(shapeURL)).toBeUndefined();
+  });
 });
