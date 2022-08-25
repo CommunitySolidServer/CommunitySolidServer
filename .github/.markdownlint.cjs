4ce30cbd897e2948489611efa43b0121e2ba2b0a
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2022-08-25 11:17:52 +0200

    chore: implement requested changes

--- .github/.markdownlint.cjs
+++
@@ -1,8 +0,0 @@
-
-var options = require('../.markdownlint-cli2.cjs');
-"use strict";
-
-module.exports = {
-  ...options.config,
-  "MD041": false
-};
--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -1,32 +1,31 @@
 "use strict";
 
 module.exports = {
-  "ignores": [ "node_modules/", "LICENSE.md" ],
+  ignores: [ "node_modules/", "LICENSE.md" ],
 
-  "globs": [ "**/*.md" ],
+  globs: [ "**/*.md" ],
 
-  "config": {
+  config: {
     // Enable all markdownlint rules
-    "default": true,
+    default: true,
 
     // Set list indent level to 4 which mkdocs / Python-Markdown requires
-    "MD007": { "indent": 4 },
+    MD007: { "indent": 4 },
 
     // Enable line length check but exclude tables and code blocks
-    "MD013": {
-      "line_length": 120,
-      "tables": false,
-      "code_blocks": false
+    MD013: {
+      line_length: 120,
+      tables: false,
+      code_blocks: false
     },
 
     // Allow multiple subheadings with the same content
     // across different section (#1 ##A ##B #2 ##A ##B)
-    "MD024": {
-      "allow_different_nesting": true
+    MD024: {
+      allow_different_nesting: true
     },
 
     // Set Ordered list item prefix to "ordered" (use 1. 2. 3. not 1. 1. 1.)
-    "MD029": { "style": "ordered" }
-
+    MD029: { "style": "ordered" }
   }
 };
