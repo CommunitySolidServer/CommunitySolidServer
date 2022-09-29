3daf7b1c54e9fc6804d8bfe056bee537122953e5
Joachim Van Herwegen <joachimvh@gmail.com>
2022-09-29 16:46:46 +0200

    feat: Create an OperationRouterHandler
    This allows us to route after an Operation has been parsed

---
+++ src/server/util/OperationRouterHandler.ts
@@ -0,0 +1,16 @@
+import type { OperationHttpHandlerInput, OperationHttpHandler } from '../OperationHttpHandler';
+import type { BaseRouterHandlerArgs } from './BaseRouterHandler';
+import { BaseRouterHandler } from './BaseRouterHandler';
+
+/**
+ * A {@link BaseRouterHandler} for an {@link OperationHttpHandler}.
+ */
+export class OperationRouterHandler extends BaseRouterHandler<OperationHttpHandler> {
+  public constructor(args: BaseRouterHandlerArgs<OperationHttpHandler>) {
+    super(args);
+  }
+
+  public async canHandle(input: OperationHttpHandlerInput): Promise<void> {
+    await super.canHandleInput(input, input.operation.method, input.operation.target);
+  }
+}
