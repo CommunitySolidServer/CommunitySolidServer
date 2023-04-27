7a4170a0f5d17a629e7517ebfd214e7f8dc6baa6
Joachim Van Herwegen <joachimvh@gmail.com>
2023-04-27 09:49:19 +0200

    test: Fix Jest memory issues

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,38 @@
+const os = require('os');
+const v8 = require('v8');
+
+// Several parts inspired by https://github.com/renovatebot/renovate/blob/main/package.json
+
+const ci = !!process.env.CI;
+
+const cpus = os.cpus();
+const mem = os.totalmem();
+const stats = v8.getHeapStatistics();
+
+if (ci) {
+  process.stderr.write(`Host stats:
+  Cpus:      ${cpus.length}
+  Memory:    ${(mem / 1024 / 1024 / 1024).toFixed(2)} GB
+  HeapLimit: ${(stats.heap_size_limit / 1024 / 1024 / 1024).toFixed(2)} GB
+`);
+}
+
+// See also https://github.com/jestjs/jest/issues/11956
+function jestGithubRunnerSpecs() {
+  return {
+    maxWorkers: cpus.length,
+    workerIdleMemoryLimit: '1500MB',
+  };
+}
+
 module.exports = {
+  transform: {
+    '^.+\\.ts$': [ 'ts-jest', {
+        tsconfig: '<rootDir>/tsconfig.json',
+        diagnostics: false,
+        isolatedModules: true,
+    }],
+  },
   testRegex: '/test/(unit|integration)/.*\\.test\\.ts$',
   moduleFileExtensions: [
     'ts',
@@ -13,5 +47,7 @@ module.exports = {
   },
   // Slower machines had problems calling the WebSocket integration callbacks on time
   testTimeout: 60000,
-  preset: 'ts-jest',
+
+  reporters: ci ? ['default', 'github-actions'] : ['default'],
+  ...(ci && jestGithubRunnerSpecs()),
 };
