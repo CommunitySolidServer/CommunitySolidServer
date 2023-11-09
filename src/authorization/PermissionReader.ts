fe4001fa238043e8f798d3aea193c2e1b42412ca
Wouter Termont <woutermont@gmail.com>
2023-11-09 13:00:28 +0100

    docs: corrections to code documentation
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

--- src/authorization/PermissionReader.ts
+++ src/authorization/PermissionReader.ts
@@ -16,7 +16,7 @@ export interface PermissionReaderInput<TCredentials extends Record<string, unkno
 
 /**
  * Discovers the permissions of the given credentials on the given identifier.
- * In case the reader finds no permission for the requested identifiers and credentials
+ * If the reader finds no permission for the requested identifiers and credentials,
  * it can return an empty or incomplete map.
  */
 export abstract class PermissionReader<TCredentials extends Record<string, unknown> = Credentials>
