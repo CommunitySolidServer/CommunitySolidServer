ba18f62726eff1dd0c5c0138625ce51b103c7ba2
elf Pavlik <elf-pavlik@hackers4peace.net>
2024-08-14 11:35:16 -0600

    clean up urls and routing

--- test/unit/server/notifications/StreamingHttpChannel2023/StreamingHttpMetadataWriter.test.ts
+++ test/unit/server/notifications/StreamingHttpChannel2023/StreamingHttpMetadataWriter.test.ts
@@ -2,14 +2,17 @@ import { createResponse } from 'node-mocks-http';
 import {
   StreamingHttpMetadataWriter,
 } from '../../../../../src/server/notifications/StreamingHttpChannel2023/StreamingHttpMetadataWriter';
+import {
+  AbsolutePathInteractionRoute,
+} from '../../../../../src/identity/interaction/routing/AbsolutePathInteractionRoute';
 import { RepresentationMetadata } from '../../../../../src/http/representation/RepresentationMetadata';
 import type { HttpResponse } from '../../../../../src/server/HttpResponse';
 import type { ResourceIdentifier } from '../../../../../src/http/representation/ResourceIdentifier';
 
 describe('A StreamingHttpMetadataWriter', (): void => {
-  const baseUrl = 'http://example.org/';
-  const pathPrefix = '.notifications/StreamingHTTPChannel2023/';
-  const writer = new StreamingHttpMetadataWriter(baseUrl, pathPrefix);
+  const path = 'http://example.org/.notifications/StreamingHTTPChannel2023/';
+  const route = new AbsolutePathInteractionRoute(path);
+  const writer = new StreamingHttpMetadataWriter(route);
   const rel = 'http://www.w3.org/ns/solid/terms#updatesViaStreamingHttp2023';
 
   it('adds the correct link header.', async(): Promise<void> => {
