616a238e6e7ff42d75069074667a5e5d4cfcb2d5
Joachim Van Herwegen <joachimvh@gmail.com>
2025-03-27 10:33:02 +0100

    refactor: Add generics to AccountStore so it can be more easily extended

--- src/identity/interaction/account/util/BaseAccountStore.ts
+++ src/identity/interaction/account/util/BaseAccountStore.ts
@@ -1,64 +1,19 @@
-import { Initializer } from '../../../../init/Initializer';
-import { getLoggerFor } from '../../../../logging/LogUtil';
-import type { ValueType } from '../../../../storage/keyvalue/IndexedStorage';
-import { createErrorMessage } from '../../../../util/errors/ErrorUtil';
-import { InternalServerError } from '../../../../util/errors/InternalServerError';
-import type { AccountSettings, AccountStore } from './AccountStore';
+import type { MinimalAccountSettings } from './AccountStore';
 import { ACCOUNT_SETTINGS_REMEMBER_LOGIN } from './AccountStore';
+import { GenericAccountStore } from './GenericAccountStore';
 import type { AccountLoginStorage } from './LoginStorage';
-import { ACCOUNT_TYPE } from './LoginStorage';
 
 export const ACCOUNT_STORAGE_DESCRIPTION = {
   [ACCOUNT_SETTINGS_REMEMBER_LOGIN]: 'boolean?',
 } as const;
 
 /**
- * A {@link AccountStore} that uses an {@link AccountLoginStorage} to keep track of the accounts.
+ * A {@link GenericAccountStore} that supports the minimal account settings.
  * Needs to be initialized before it can be used.
  */
-export class BaseAccountStore extends Initializer implements AccountStore {
-  private readonly logger = getLoggerFor(this);
-
-  private readonly storage: AccountLoginStorage<{ [ACCOUNT_TYPE]: typeof ACCOUNT_STORAGE_DESCRIPTION }>;
-  private initialized = false;
-
+export class BaseAccountStore extends GenericAccountStore<MinimalAccountSettings> {
   // Wrong typings to prevent Components.js typing issues
   public constructor(storage: AccountLoginStorage<Record<string, never>>) {
-    super();
-    this.storage = storage as unknown as typeof this.storage;
-  }
-
-  // Initialize the type definitions
-  public async handle(): Promise<void> {
-    if (this.initialized) {
-      return;
-    }
-    try {
-      await this.storage.defineType(ACCOUNT_TYPE, ACCOUNT_STORAGE_DESCRIPTION, false);
-      this.initialized = true;
-    } catch (cause: unknown) {
-      throw new InternalServerError(`Error defining account in storage: ${createErrorMessage(cause)}`, { cause });
-    }
-  }
-
-  public async create(): Promise<string> {
-    const { id } = await this.storage.create(ACCOUNT_TYPE, {});
-    this.logger.debug(`Created new account ${id}`);
-
-    return id;
-  }
-
-  public async getSetting<T extends keyof AccountSettings>(id: string, setting: T): Promise<AccountSettings[T]> {
-    const account = await this.storage.get(ACCOUNT_TYPE, id);
-    if (!account) {
-      return;
-    }
-    const { id: unused, ...settings } = account;
-    return settings[setting];
-  }
-
-  public async updateSetting<T extends keyof AccountSettings>(id: string, setting: T, value: AccountSettings[T]):
-  Promise<void> {
-    await this.storage.setField(ACCOUNT_TYPE, id, setting, value as ValueType<typeof ACCOUNT_STORAGE_DESCRIPTION[T]>);
+    super(storage, ACCOUNT_STORAGE_DESCRIPTION);
   }
 }
--- test/unit/identity/interaction/account/util/BaseAccountStore.test.ts
+++ test/unit/identity/interaction/account/util/BaseAccountStore.test.ts
@@ -6,21 +6,14 @@ import type {
 import {
   ACCOUNT_TYPE,
 } from '../../../../../../src/identity/interaction/account/util/LoginStorage';
-import { InternalServerError } from '../../../../../../src/util/errors/InternalServerError';
-
-jest.mock('uuid', (): any => ({ v4: (): string => '4c9b88c1-7502-4107-bb79-2a3a590c7aa3' }));
 
 describe('A BaseAccountStore', (): void => {
-  const id = 'id';
   let storage: jest.Mocked<AccountLoginStorage<any>>;
   let store: BaseAccountStore;
 
   beforeEach(async(): Promise<void> => {
     storage = {
       defineType: jest.fn().mockResolvedValue({}),
-      create: jest.fn().mockResolvedValue({ id }),
-      get: jest.fn().mockResolvedValue({ id, [ACCOUNT_SETTINGS_REMEMBER_LOGIN]: true }),
-      setField: jest.fn(),
     } satisfies Partial<AccountLoginStorage<any>> as any;
 
     store = new BaseAccountStore(storage);
@@ -32,41 +25,5 @@ describe('A BaseAccountStore', (): void => {
     expect(storage.defineType).toHaveBeenLastCalledWith(ACCOUNT_TYPE, {
       [ACCOUNT_SETTINGS_REMEMBER_LOGIN]: 'boolean?',
     }, false);
-  });
-
-  it('can only initialize once.', async(): Promise<void> => {
-    await expect(store.handle()).resolves.toBeUndefined();
-    await expect(store.handle()).resolves.toBeUndefined();
-    expect(storage.defineType).toHaveBeenCalledTimes(1);
-  });
-
-  it('throws an error if defining the type goes wrong.', async(): Promise<void> => {
-    storage.defineType.mockRejectedValueOnce(new Error('bad data'));
-    await expect(store.handle()).rejects.toThrow(InternalServerError);
-  });
-
-  it('creates an empty account.', async(): Promise<void> => {
-    await expect(store.create()).resolves.toEqual(id);
-    expect(storage.create).toHaveBeenCalledTimes(1);
-    expect(storage.create).toHaveBeenLastCalledWith(ACCOUNT_TYPE, {});
-  });
-
-  it('can return a setting.', async(): Promise<void> => {
-    await expect(store.getSetting(id, ACCOUNT_SETTINGS_REMEMBER_LOGIN)).resolves.toBe(true);
-    expect(storage.get).toHaveBeenCalledTimes(1);
-    expect(storage.get).toHaveBeenLastCalledWith(ACCOUNT_TYPE, id);
-  });
-
-  it('returns undefined if the accountId is invalid.', async(): Promise<void> => {
-    storage.get.mockResolvedValueOnce(undefined);
-    await expect(store.getSetting(id, ACCOUNT_SETTINGS_REMEMBER_LOGIN)).resolves.toBeUndefined();
-    expect(storage.get).toHaveBeenCalledTimes(1);
-    expect(storage.get).toHaveBeenLastCalledWith(ACCOUNT_TYPE, id);
-  });
-
-  it('can set the settings.', async(): Promise<void> => {
-    await expect(store.updateSetting(id, ACCOUNT_SETTINGS_REMEMBER_LOGIN, true)).resolves.toBeUndefined();
-    expect(storage.setField).toHaveBeenCalledTimes(1);
-    expect(storage.setField).toHaveBeenLastCalledWith(ACCOUNT_TYPE, id, ACCOUNT_SETTINGS_REMEMBER_LOGIN, true);
   });
 });
